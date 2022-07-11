5ef8fe4afa88686a6768ec2570fdeebb20e32b0f
Harshvardhantomar <harshvardhant789@gmail.com>
2022-07-11 11:47:29 +0530

    Removed utbot-fuzzers module

--- utbot-fuzzers/src/main/kotlin/org/utbot/fuzzer/ObsoleteTestCaseGenerator.kt
+++
@@ -1,9 +0,0 @@
-package org.utbot.fuzzer
-
-import org.utbot.framework.plugin.api.MockStrategyApi
-import org.utbot.framework.plugin.api.UtMethod
-import org.utbot.framework.plugin.api.UtValueTestCase
-
-interface ObsoleteTestCaseGenerator {
-    fun generate(method: UtMethod<*>, mockStrategy: MockStrategyApi): UtValueTestCase<*>
-}
--- utbot-fuzzers/src/main/kotlin/org/utbot/fuzzer/names/TestSuggestedInfo.kt
+++
@@ -1,9 +0,0 @@
-package org.utbot.fuzzer.names
-
-/**
- * Information that can be used to generate test names.
- */
-class TestSuggestedInfo(
-    val testName: String,
-    val displayName: String? = null
-)

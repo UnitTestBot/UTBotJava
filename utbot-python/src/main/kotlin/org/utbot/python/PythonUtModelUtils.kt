dea3e34edf1c50ead23ea85ca6d03e6e90c5fdb4
Ekaterina Tochilina <katerina_t_n@mail.ru>
2022-07-15 10:33:10 +0300

    Added PythonModel

---
+++ utbot-python/src/main/kotlin/org/utbot/python/PythonUtModelUtils.kt
@@ -0,0 +1,12 @@
+package org.utbot.python
+
+import org.utbot.framework.plugin.api.ClassId
+import org.utbot.framework.plugin.api.UtModel
+
+class PythonModel (
+    val model: UtModel
+) {
+    fun render(): String {
+        return ""
+    }
+}

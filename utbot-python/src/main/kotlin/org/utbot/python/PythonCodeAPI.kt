4e725679df01b976491ea1cff84e1a4032893cff
Vyacheslav Tamarin <tamarin.vs@ya.ru>
2022-07-18 18:09:34 +0300

    Move PyhtonTestCodeGenerator to PythonCodeAPI and rewrite with AST using

--- utbot-python/src/main/kotlin/org/utbot/python/PythonCodeAPI.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/PythonCodeAPI.kt
@@ -4,13 +4,19 @@ import io.github.danielnaczo.python3parser.Python3Lexer
 import io.github.danielnaczo.python3parser.Python3Parser
 import io.github.danielnaczo.python3parser.model.AST
 import io.github.danielnaczo.python3parser.model.expr.Expression
+import io.github.danielnaczo.python3parser.model.expr.atoms.Atom
 import io.github.danielnaczo.python3parser.model.expr.atoms.Name
 import io.github.danielnaczo.python3parser.model.expr.atoms.Num
 import io.github.danielnaczo.python3parser.model.expr.atoms.Str
+import io.github.danielnaczo.python3parser.model.expr.atoms.trailers.arguments.Arguments
+import io.github.danielnaczo.python3parser.model.expr.atoms.trailers.arguments.Keyword
+import io.github.danielnaczo.python3parser.model.expr.operators.binaryops.comparisons.Eq
 import io.github.danielnaczo.python3parser.model.mods.Module
 import io.github.danielnaczo.python3parser.model.stmts.compoundStmts.ClassDef
 import io.github.danielnaczo.python3parser.model.stmts.compoundStmts.functionStmts.FunctionDef
 import io.github.danielnaczo.python3parser.model.stmts.compoundStmts.functionStmts.parameters.Parameter
+import io.github.danielnaczo.python3parser.model.stmts.smallStmts.Assert
+import io.github.danielnaczo.python3parser.model.stmts.smallStmts.assignStmts.Assign
 import io.github.danielnaczo.python3parser.visitors.ast.ModuleVisitor
 import io.github.danielnaczo.python3parser.visitors.modifier.ModifierVisitor
 import io.github.danielnaczo.python3parser.visitors.prettyprint.IndentationPrettyPrint
@@ -26,6 +32,7 @@ import org.utbot.framework.plugin.api.util.intClassId
 import org.utbot.framework.plugin.api.util.longClassId
 import org.utbot.framework.plugin.api.util.stringClassId
 import org.utbot.fuzzer.FuzzedConcreteValue
+import java.io.File
 import java.util.*
 import java.util.function.Consumer
 import javax.xml.bind.DatatypeConverter.parseLong
@@ -127,4 +134,72 @@ class PythonMethodBody(private val ast: FunctionDef): PythonMethod {
         fun annotationToString(annotation: Optional<Expression>): String? =
             if (annotation.isPresent) (annotation.get() as? Name)?.id?.name else null
     }
+}
+
+object PythonCodeGenerator {
+    fun generateTestCode(testCase: PythonTestCase): List<String> {
+        return testCase.executions.mapIndexed { index, utExecution ->
+            generateTestCode(testCase.method, utExecution, index)
+        }
+    }
+
+    fun generateTestCode(method: PythonMethod, execution: UtExecution, number: Int): String {
+        val testFunctionName = "${execution.testMethodName?.camelToSnakeCase() ?: "test"}_$number"
+        val testFunction = FunctionDef(testFunctionName)
+
+        val parameters = execution.stateBefore.parameters.zip(method.arguments).map { (model, argument) ->
+            Assign(
+                listOf(Name("${argument.name}_")),
+                Name(model.toString())
+            )
+        }
+        parameters.forEach {
+            testFunction.addStatement(it)
+        }
+
+        val actualName = "actual"
+        val keywords = method.arguments.map {
+            Keyword(Name(it.name), Name("${it.name}_"))
+        }
+        val functionCall = Assign(
+            listOf(Name(actualName)),
+            Atom(
+                Name(method.name),
+                listOf(
+                    Arguments(emptyList(), keywords, emptyList(), emptyList())
+                )
+            )
+        )
+        testFunction.addStatement(functionCall)
+
+        val correctResultName = "correct_result"
+        val correctResult = Assign(
+            listOf(Name(correctResultName)),
+            Name(execution.result.toString())
+        )
+        testFunction.addStatement(correctResult)
+
+        val assertLine = Assert(
+            Eq(
+                Name(correctResultName),
+                Name(actualName)
+            )
+        )
+        testFunction.addStatement(assertLine)
+
+        return PythonMethodBody(testFunction).asString()
+    }
+
+    fun saveToFile(filePath: String, code: List<String>) {
+        val file = File(filePath)
+        file.writeText(code.joinToString("\n\n\n"))
+        file.createNewFile()
+    }
+}
+
+fun String.camelToSnakeCase(): String {
+    val camelRegex = "(?<=[a-zA-Z])[A-Z]".toRegex()
+    return camelRegex.replace(this) {
+        "_${it.value}"
+    }.toLowerCase()
 }

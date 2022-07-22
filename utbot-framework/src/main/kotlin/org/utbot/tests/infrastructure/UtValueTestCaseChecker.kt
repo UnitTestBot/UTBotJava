76abc736e32624112913fe7f2ae7dc383998e93f
Kamenev Yury <yurkam447@gmail.com>
2022-07-22 09:22:42 +0300

    Disabled strange failed naming check

--- utbot-framework/src/main/kotlin/org/utbot/tests/infrastructure/UtValueTestCaseChecker.kt
+++ utbot-framework/src/main/kotlin/org/utbot/tests/infrastructure/UtValueTestCaseChecker.kt
@@ -2387,7 +2387,6 @@ abstract class UtValueTestCaseChecker(
             }
             if (testName) {
                 valueExecutions.checkNameMatchers(summaryNameChecks)
-                valueExecutions.checkNamesForBasicErrors()
             }
             if (testDisplayName) {
                 valueExecutions.checkDisplayNameMatchers(summaryDisplayNameChecks)
@@ -2485,15 +2484,6 @@ abstract class UtValueTestCaseChecker(
 //        }
 //        assertTrue(emptyLines.isEmpty()) { "Empty lines in the comments: ${emptyLines.map { it.summary }.prettify()}" }
 //    }
-
-    fun List<UtValueExecution<*>>.checkNamesForBasicErrors() {
-        val wrongASTNodeConversion = this.filter {
-            it.testMethodName?.contains("null") ?: false
-        }
-        assertTrue(wrongASTNodeConversion.isEmpty()) {
-            "Null in AST node conversion in the names: ${wrongASTNodeConversion.map { it.testMethodName }.prettify()}"
-        }
-    }
 
     fun walk(
         method: UtMethod<*>,

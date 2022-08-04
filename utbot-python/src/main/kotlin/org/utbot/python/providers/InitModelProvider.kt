4e91215a83ce2158634a05ef4cb4c1492b61c82e
Ekaterina Tochilina <katerina_t_n@mail.ru>
2022-08-04 16:02:15 +0300

    fixed bug with empty constructor

--- utbot-python/src/main/kotlin/org/utbot/python/providers/InitModelProvider.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/providers/InitModelProvider.kt
@@ -1,6 +1,7 @@
 package org.utbot.python.providers
 
 import org.utbot.framework.plugin.api.PythonInitObjectModel
+import org.utbot.framework.plugin.api.PythonModel
 import org.utbot.fuzzer.*
 import org.utbot.python.typing.PythonTypesStorage
 
@@ -11,16 +12,24 @@ object InitModelProvider: ModelProvider {
         description.parametersMap.forEach { (classId, parameterIndices) ->
 
             val type = PythonTypesStorage.getTypeByName(classId) ?: return@forEach
-            val constructor = FuzzedMethodDescription(
-                type.name,
-                classId,
-                type.initSignature ?: return@forEach,
-                description.concreteValues
-            )
+            val initSignature = type.initSignature ?: return@forEach
 
-            val models = fuzz(constructor, nonRecursiveModelProvider).map { initValues ->
-                PythonInitObjectModel(classId.name, initValues.map { it.model })
-            }
+            val models: Sequence<PythonModel> =
+                if (initSignature.isEmpty())
+                    sequenceOf(PythonInitObjectModel(classId.name, emptyList()))
+                else {
+                    val constructor = FuzzedMethodDescription(
+                        type.name,
+                        classId,
+                        initSignature,
+                        description.concreteValues
+                    )
+
+                    fuzz(constructor, nonRecursiveModelProvider).map { initValues ->
+                        PythonInitObjectModel(classId.name, initValues.map { it.model })
+                    }
+                }
+
             parameterIndices.forEach { index ->
                 models.forEach { model ->
                     yield(FuzzedParameter(index, model.fuzzed()))

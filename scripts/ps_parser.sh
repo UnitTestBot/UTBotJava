b9f831be04ec9b694770f3282a109ee118b95e8d
Egor Vasilyev <e.s.vasilyev@mail.ru>
2022-08-09 16:14:41 +0300

    monitor only Java

--- scripts/ps_parser.sh
+++ scripts/ps_parser.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 while read line; do
+	#echo $line
 	PID=$(echo $line | awk '{ print $1 }')
 	RSS=$(echo $line | awk '{ print $2 * 1024 }')
 	PID_EXECUTABLE=$(cat /proc/${PID}/stat | awk '{ print $2 }' | sed -n 's/^(\(.*\))$/\1/p' )
 	DESCRIPTION=$(echo $line | grep -o "Gradle Test Executor [0-9]*")
-	echo "processe_memory_bytes{pid=\"${PID}\",pid_executable=\"${PID_EXECUTABLE}\",description=\"${DESCRIPTION}\"} ${RSS}"
-done <<< $(ps -ax --no-headers --format=pid,rss,command --sort=-rss,pid | grep -v ps_parser.sh)
+	if [[ "${PID_EXECUTABLE=}" == "java" ]]; then
+		echo "processe_memory_bytes{pid=\"${PID}\",pid_executable=\"${PID_EXECUTABLE}\",description=\"${DESCRIPTION}\"} ${RSS}"
+	fi
+done <<< $(ps -ax --no-headers --format=pid,rss,command --sort=-rss,pid)

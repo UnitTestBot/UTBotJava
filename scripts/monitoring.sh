2c9a0a8a8a34b2f76125fe46c768d2c38a538f03
Egor Vasilyev <e.s.vasilyev@mail.ru>
2022-08-11 11:53:15 +0300

    port checking added

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -15,35 +15,42 @@ SLEEP_TIME_SECONDS=15
 VERSION_CADVISOR=v0.36.0
 VERSION_CURL=7.84.0
 VERSION_NODE_EXPORTER=v1.3.1
+PORT_CADVISOR=9280
+PORT_NODE_EXPORTER=9100
 
 # container metrics
-docker run -d --name cadvisor \
-              --volume=/:/rootfs:ro \
-              --volume=/var/run:/var/run:ro \
-              --volume=/sys:/sys:ro \
-              --volume=/var/lib/docker/:/var/lib/docker:ro \
-              --volume=/dev/disk/:/dev/disk:ro \
-              --publish=9280:8080 \
-              --privileged \
-              --device=/dev/kmsg \
-                  gcr.io/cadvisor/cadvisor:${VERSION_CADVISOR}
-docker run -d --name curl-container \
-              --net="host" \
-              --entrypoint=/bin/sh \
-                  curlimages/curl:${VERSION_CURL} \
+if lsof -Pi :${PORT_CADVISOR} -sTCP:LISTEN ; then
+  docker run -d --name cadvisor \
+                --volume=/:/rootfs:ro \
+                --volume=/var/run:/var/run:ro \
+                --volume=/sys:/sys:ro \
+                --volume=/var/lib/docker/:/var/lib/docker:ro \
+                --volume=/dev/disk/:/dev/disk:ro \
+                --publish=9280:8080 \
+                --privileged \
+                --device=/dev/kmsg \
+                    gcr.io/cadvisor/cadvisor:${VERSION_CADVISOR}
+  docker run -d --name curl-container \
+                --net="host" \
+                --entrypoint=/bin/sh \
+                    curlimages/curl:${VERSION_CURL} \
                   "-c" "while true; do curl localhost:9280/metrics | grep -v 'id=\"\/\(system\|user\).slice' | sed -r 's/(^.*} .*) ([0-9]*)/\1/' | curl -u ${PUSHGATEWAY_USER}:${PUSHGATEWAY_PASSWORD} --data-binary @- https://${PUSHGATEWAY_HOSTNAME}${PUSHGATEWAY_ADDITIONAL_PATH}/metrics/job/pushgateway/instance/${GITHUB_RUN_ID}-${HOSTNAME}${PROM_ADDITIONAL_LABELS} ; sleep ${SLEEP_TIME_SECONDS}; done"
+fi
+
 # base linux system metrics
-docker run -d --name node_exporter \
-              --net="host" \
-              --pid="host" \
-              --volume="/:/host:ro,rslave" \
-                  quay.io/prometheus/node-exporter:${VERSION_NODE_EXPORTER} \
-                  --path.rootfs=/host
-docker run -d --name curl-node \
-              --net="host" \
-              --entrypoint=/bin/sh \
-                  curlimages/curl:${VERSION_CURL} \
+if lsof -Pi :${PORT_NODE_EXPORTER} -sTCP:LISTEN ; then
+  docker run -d --name node_exporter \
+                --net="host" \
+                --pid="host" \
+                --volume="/:/host:ro,rslave" \
+                    quay.io/prometheus/node-exporter:${VERSION_NODE_EXPORTER} \
+                    --path.rootfs=/host
+  docker run -d --name curl-node \
+                --net="host" \
+                --entrypoint=/bin/sh \
+                    curlimages/curl:${VERSION_CURL} \
                   "-c" "while true; do curl localhost:9100/metrics | curl -u ${PUSHGATEWAY_USER}:${PUSHGATEWAY_PASSWORD} --data-binary @- https://${PUSHGATEWAY_HOSTNAME}${PUSHGATEWAY_ADDITIONAL_PATH}/metrics/job/pushgateway/instance/${GITHUB_RUN_ID}-${HOSTNAME}${PROM_ADDITIONAL_LABELS} ; sleep ${SLEEP_TIME_SECONDS}; done"
+fi
 
 # custom java processes memory metrics
 chmod +x scripts/ps_parser.sh

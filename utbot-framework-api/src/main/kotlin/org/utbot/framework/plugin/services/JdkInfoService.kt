c38666ceb30f7c76c09364a7e09b2cdb92a046b8
Sergey Pospelov <sergeypospelov59@gmail.com>
2022-08-11 12:00:07 +0300

    Fix comment

--- utbot-framework-api/src/main/kotlin/org/utbot/framework/plugin/services/JdkInfoService.kt
+++ utbot-framework-api/src/main/kotlin/org/utbot/framework/plugin/services/JdkInfoService.kt
@@ -3,18 +3,19 @@ package org.utbot.framework.plugin.services
 import java.nio.file.Path
 import java.nio.file.Paths
 
-/**
- * Singleton to enable abstract access to path to JDK.
-
- * and in the test runs.
- * This is necessary because the engine can be run from the various starting points, like IDEA plugin, CLI, etc.
- */
 data class JdkInfo(
     val path: Path,
     @Suppress("unused")
     val version: String
 )
 
+/**
+ * Singleton to enable abstract access to path to JDK.
+
+ * Used in [org.utbot.instrumentation.process.ChildProcessRunner].
+ * The purpose is to use the same JDK in [org.utbot.instrumentation.ConcreteExecutor] and in the test runs.
+ * This is necessary because the engine can be run from the various starting points, like IDEA plugin, CLI, etc.
+ */
 object JdkInfoService : PluginService<JdkInfo> {
     var jdkInfoProvider: JdkInfoProvider = JdkInfoDefaultProvider()
 

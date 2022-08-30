e0b2fc88045d8dc780d6f8141685892ebb5bc9d3
Amandel Pie <105506115+amandelpie@users.noreply.github.com>
2022-08-30 14:16:11 +0300

    Refactored scripts location (#815)

--- scripts/ml/train_data.sh
+++ scripts/ml/train_data.sh
@@ -9,6 +9,6 @@ while read prog; do
   while read selector; do
     echo "Starting features collection from $prog with $selector"
     selector="${selector%%[[:cntrl:]]}"
-    $WORKDIR/scripts/run_contest_estimator.sh "$prog" "$TIME_LIMIT" "$selector" true
-  done <"$WORKDIR/scripts/selector_list"
-done <"$WORKDIR/scripts/prog_list"
+    $WORKDIR/scripts/ml/run_contest_estimator.sh "$prog" "$TIME_LIMIT" "$selector" true
+  done <"$WORKDIR/scripts/ml/selector_list"
+done <"$WORKDIR/scripts/ml/prog_list"

e6d3f9b4bda91a7afb7eb01b0f1341af2b73bee4
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2022-09-07 14:34:00 +0300

    Do not generate JavaDocs for empty comments #280 (#866)
    * Do not generate JavaDocs for empty comments #280
    
    * Fix broken tests #280
    
    * Review fixes #280

--- utbot-summary/src/main/kotlin/org/utbot/summary/comment/SimpleClusterCommentBuilder.kt
+++ utbot-summary/src/main/kotlin/org/utbot/summary/comment/SimpleClusterCommentBuilder.kt
@@ -33,9 +33,11 @@ class SimpleClusterCommentBuilder(
         skippedIterations()
         buildSentenceBlock(traceTag.rootStatementTag, root, currentMethod)
         var sentence = toSentence(root)
+
         if (sentence.isEmpty()) {
-            return genWarnNotification()
+            return EMPTY_STRING
         }
+
         sentence = splitLongSentence(sentence)
         sentence = lastCommaToDot(sentence)
 
@@ -50,7 +52,7 @@ class SimpleClusterCommentBuilder(
         val sentence = toDocStmts(root)
 
         if (sentence.isEmpty()) {
-            return listOf(DocRegularStmt(genWarnNotification())) //TODO SAT-1310
+            return emptyList()
         }
 //        sentence = splitLongSentence(sentence) //TODO SAT-1309
 //        sentence = lastCommaToDot(sentence) //TODO SAT-1309

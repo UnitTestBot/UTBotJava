7d3bcf14b66b04fe0b34930daac33bc900d2a86f
Dmitrii Timofeev <dtim@comitative.com>
2022-09-23 16:49:43 +0300

    * Updates for java11 tests

--- utbot-framework-test-java11/src/main/java/org/utbot/examples/java11/collections/ToArrayWithGenerator.java
+++ utbot-framework-test-java11/src/main/java/org/utbot/examples/java11/collections/ToArrayWithGenerator.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
+@SuppressWarnings("Since15")
 public class ToArrayWithGenerator<T> {
     @SuppressWarnings("MismatchedReadAndWriteOfArray")
     public boolean checkSetSize(int size) {

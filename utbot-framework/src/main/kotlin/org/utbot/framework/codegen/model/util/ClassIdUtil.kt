38fb0d0240ca59b615c04d41eab78a5e19ba840b
Ivan Volkov <volkov.ivan2004@gmail.com>
2022-09-27 18:21:35 +0300

    Minor changes fixing issues found in review

--- utbot-framework/src/main/kotlin/org/utbot/framework/codegen/model/util/ClassIdUtil.kt
+++ utbot-framework/src/main/kotlin/org/utbot/framework/codegen/model/util/ClassIdUtil.kt
@@ -36,11 +36,11 @@ infix fun ClassId.isAccessibleFrom(packageName: String): Boolean {
 /**
  * Returns field of [this], such that [methodId] is a getter for it (or null if methodId doesn't represent a getter)
  */
-internal fun ClassId.fieldThisIsGetterFor(methodId: MethodId): FieldId? =
-    allDeclaredFieldIds.firstOrNull { !it.isStatic && it.getter == methodId }
+internal fun ClassId.fieldThatIsGotWith(methodId: MethodId): FieldId? =
+    allDeclaredFieldIds.singleOrNull { !it.isStatic && it.getter == methodId }
 
 /**
  * Returns field of [this], such that [methodId] is a setter for it (or null if methodId doesn't represent a setter)
  */
-internal fun ClassId.fieldThisIsSetterFor(methodId: MethodId): FieldId? =
-    allDeclaredFieldIds.firstOrNull { !it.isStatic && it.setter == methodId }
+internal fun ClassId.fieldThatIsSetWith(methodId: MethodId): FieldId? =
+    allDeclaredFieldIds.singleOrNull { !it.isStatic && it.setter == methodId }

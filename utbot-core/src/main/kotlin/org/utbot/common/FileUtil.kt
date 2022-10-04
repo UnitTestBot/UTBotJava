bcc37a27692b8bb655d0219bbaaf8da647298b04
Vassiliy.Kudryashov <Vassiliy.Kudryashov@gmail.com>
2022-10-04 20:51:44 +0300

    We create temporary files a lot, we have to clean it either

--- utbot-core/src/main/kotlin/org/utbot/common/FileUtil.kt
+++ utbot-core/src/main/kotlin/org/utbot/common/FileUtil.kt
@@ -72,12 +72,18 @@ object FileUtil {
             val creationTime = Files.readAttributes(it.toPath(), BasicFileAttributes::class.java).creationTime()
             TimeUnit.MILLISECONDS.toDays(currentTimeInMillis - creationTime.toMillis()) > daysLimit
         }.forEach { it.deleteRecursively() }
-        File(tempDirectoryPath).listFiles { _, name ->
-            name.matches(Regex("Graph-vis\\d{10,}"))
-                    || name.matches(Regex("\\d{10,}-0"))
-                    || name.matches(Regex("byteBuddyAgent\\d{10,}\\.jar"))
-                    || name.matches(Regex("cmd-args\\d{10,}"))
-                    || name.matches(Regex("mockitoboot\\d{10,}\\.jar"))
+        File(tempDirectoryPath).listFiles { file ->
+            val name = file.name
+            if ((name.matches(Regex("Graph-vis\\d{10,}"))
+                        || name.matches(Regex("\\d{10,}-0"))
+                        || name.matches(Regex("byteBuddyAgent\\d{10,}\\.jar"))
+                        || name.matches(Regex("cmd-args\\d{10,}"))
+                        || name.matches(Regex("mockitoboot\\d{10,}\\.jar")))
+            ) {
+                val creationTime = Files.readAttributes(file.toPath(), BasicFileAttributes::class.java).creationTime()
+                return@listFiles (TimeUnit.MILLISECONDS.toDays(currentTimeInMillis - creationTime.toMillis()) > daysLimit)
+            }
+            return@listFiles false
         }?.forEach { it.deleteRecursively() }
     }
 

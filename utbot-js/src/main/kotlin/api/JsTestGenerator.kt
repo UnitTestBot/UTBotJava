657f3037f84bff0d40c9d08fa628997f1233d31d
Sergey Loktev <71882967+zishkaz@users.noreply.github.com>
2022-10-16 09:35:39 +0300

    New js coverage algo implementation

--- utbot-js/src/main/kotlin/api/JsTestGenerator.kt
+++ utbot-js/src/main/kotlin/api/JsTestGenerator.kt
@@ -7,9 +7,9 @@ import com.oracle.js.parser.ScriptEnvironment
 import com.oracle.js.parser.Source
 import com.oracle.js.parser.ir.ClassNode
 import com.oracle.js.parser.ir.FunctionNode
-import com.oracle.truffle.api.strings.TruffleString
 import fuzzer.JsFuzzer
 import fuzzer.providers.JsObjectModelProvider
+import java.io.File
 import org.utbot.framework.codegen.model.constructor.CgMethodTestSet
 import org.utbot.framework.plugin.api.EnvironmentModels
 import org.utbot.framework.plugin.api.ExecutableId
@@ -23,7 +23,6 @@ import org.utbot.framework.plugin.api.UtModel
 import org.utbot.framework.plugin.api.js.JsClassId
 import org.utbot.framework.plugin.api.js.JsMethodId
 import org.utbot.framework.plugin.api.js.JsMultipleClassId
-import org.utbot.framework.plugin.api.js.JsPrimitiveModel
 import org.utbot.framework.plugin.api.js.util.isJsBasic
 import org.utbot.framework.plugin.api.js.util.jsErrorClassId
 import org.utbot.framework.plugin.api.util.isStatic
@@ -37,20 +36,14 @@ import parser.JsFunctionAstVisitor
 import parser.JsFuzzerAstVisitor
 import parser.JsParserUtils
 import parser.JsToplevelFunctionAstVisitor
-import service.CoverageService
+import service.CoverageServiceProvider
 import service.ServiceContext
 import service.TernService
+import settings.JsDynamicSettings
 import settings.JsTestGenerationSettings.dummyClassName
-import settings.JsTestGenerationSettings.fileUnderTestAliases
-import settings.JsTestGenerationSettings.functionCallResultAnchor
-import settings.JsTestGenerationSettings.tempFileName
-import utils.JsCmdExec
 import utils.PathResolver
 import utils.constructClass
 import utils.toJsAny
-import java.io.File
-import java.util.Collections
-import settings.JsDynamicSettings
 
 
 class JsTestGenerator(
@@ -135,32 +128,35 @@ class JsTestGenerator(
         } ?: throw IllegalStateException()
         manageExports(classNode, funcNode, execId)
         val (concreteValues, fuzzedValues) = runFuzzer(funcNode, execId)
-        val coveredBranchesArray = Array<Set<Int>>(fuzzedValues.size) { emptySet() }
-        val timeoutErrors =
-            runCoverageAnalysis(context, fuzzedValues, execId, classNode, coveredBranchesArray)
-        val testsForGenerator = mutableListOf<UtExecution>()
-        val resultRegex = Regex("$functionCallResultAnchor (.*)")
-        val errorResultRegex = Regex(".*(Error: .*)")
-        val illegalStateExceptionError = if (timeoutErrors.size == fuzzedValues.size)
-            mapOf("No successful tests were generated! Please check the function under test." to 1)
-        else
-            emptyMap()
-        val errorsForGenerator = timeoutErrors.associate {
-            "Timeout in generating test for ${fuzzedValues[it].joinToString { f -> f.model.toString() }} parameters" to 1
-        } + illegalStateExceptionError
-        analyzeCoverage(coveredBranchesArray.toList()).forEach { paramIndex ->
-            val param = fuzzedValues[paramIndex]
-            val result =
-                getUtModelResult(param, execId, classNode, File(sourceFilePath).name, resultRegex, errorResultRegex)
-            val thisInstance = makeThisInstance(execId, classId, concreteValues)
-            val initEnv = EnvironmentModels(thisInstance, param.map { it.model }, mapOf())
-            testsForGenerator.add(
-                UtFuzzedExecution(
-                    stateBefore = initEnv,
-                    stateAfter = initEnv,
-                    result = result,
-                )
+        val (allCoveredStatements, executionResults) =
+            CoverageServiceProvider(context).get(
+                settings.coverageMode,
+                fuzzedValues,
+                execId,
+                classNode
             )
+        val testsForGenerator = mutableListOf<UtExecution>()
+        val errorsForGenerator = mutableMapOf<String, Int>()
+        analyzeCoverage(allCoveredStatements).forEach { paramIndex ->
+            if (executionResults[paramIndex] == "Error:Timeout") {
+                errorsForGenerator["Timeout in generating test for ${fuzzedValues[paramIndex].joinToString { f -> f.model.toString() }} parameters"] = 1
+            } else {
+                val param = fuzzedValues[paramIndex]
+                val result =
+                    getUtModelResult(
+                        execId = execId,
+                        returnText = executionResults[paramIndex]
+                    )
+                val thisInstance = makeThisInstance(execId, classId, concreteValues)
+                val initEnv = EnvironmentModels(thisInstance, param.map { it.model }, mapOf())
+                testsForGenerator.add(
+                    UtFuzzedExecution(
+                        stateBefore = initEnv,
+                        stateAfter = initEnv,
+                        result = result,
+                    )
+                )
+            }
         }
         val testSet = CgMethodTestSet(
             execId,
@@ -220,75 +216,17 @@ class JsTestGenerator(
     }
 
     private fun getUtModelResult(
-        param: List<FuzzedValue>,
         execId: JsMethodId,
-        classNode: ClassNode?,
-        importText: String,
-        resultRegex: Regex,
-        errorResultRegex: Regex
+        returnText: String
     ): UtExecutionResult {
         val utConstructor = JsUtModelConstructor()
-        val scriptText =
-            makeStringForRunJs(param, execId, classNode?.ident?.name, importText)
-        val returnText = runJs(
-            scriptText,
-            File(sourceFilePath).parent,
-        )
-        val unparsedValueSeq = resultRegex.findAll(returnText)
-        val errorSeq = errorResultRegex.findAll(returnText)
-        val unparsedValue = if (unparsedValueSeq.any()) {
-            unparsedValueSeq.last().groups[1]?.value ?: throw IllegalStateException()
-        } else {
-            errorSeq.last().groups[1]?.value ?: throw IllegalStateException()
-        }
-        val (returnValue, valueClassId) = unparsedValue.toJsAny(execId.returnType)
+        val (returnValue, valueClassId) = returnText.toJsAny(execId.returnType)
         val result = utConstructor.construct(returnValue, valueClassId)
         val utExecResult = when (result.classId) {
             jsErrorClassId -> UtExplicitlyThrownException(Throwable(returnValue.toString()), false)
             else -> UtExecutionSuccess(result)
         }
         return utExecResult
-    }
-
-    private fun runCoverageAnalysis(
-        context: ServiceContext,
-        fuzzedValues: List<List<FuzzedValue>>,
-        execId: JsMethodId,
-        classNode: ClassNode?,
-        coveredBranchesArray: Array<Set<Int>>
-    ): MutableList<Int> {
-        val timeoutErrors = Collections.synchronizedList(mutableListOf<Int>())
-        val basicCoverageService = CoverageService(
-            context = context,
-            scriptText = context.trimmedFileText,
-            id = 1024,
-            originalFileName = File(sourceFilePath).name,
-            newFileName = File(sourceFilePath).name,
-            errors = mutableListOf()
-        )
-        val basicCoverage = basicCoverageService.getCoveredLines()
-        basicCoverageService.removeTempFiles()
-        fuzzedValues.indices.toList().parallelStream().forEach {
-            val scriptText =
-                makeStringForRunJs(
-                    fuzzedValue = fuzzedValues[it],
-                    method = execId,
-                    containingClass = classNode?.ident?.name,
-                    importText = File(context.filePathToInference).name,
-                )
-            val coverageService = CoverageService(
-                context = context,
-                scriptText = scriptText,
-                id = it,
-                originalFileName = File(sourceFilePath).name,
-                newFileName = tempFileName,
-                basicCoverage = basicCoverage,
-                errors = timeoutErrors,
-            )
-            coveredBranchesArray[it] = coverageService.getCoveredLines().toSet()
-            coverageService.removeTempFiles()
-        }
-        return timeoutErrors
     }
 
     private fun runFuzzer(
@@ -305,8 +243,6 @@ class JsTestGenerator(
             }
         val fuzzedValues =
             JsFuzzer.jsFuzzing(methodUnderTestDescription = methodUnderTestDescription).toList()
-                .shuffled()
-                .take(1_000)
         return Pair(fuzzerVisitor.fuzzedConcreteValues.toSet(), fuzzedValues)
     }
 
@@ -370,76 +306,6 @@ class JsTestGenerator(
         if (!(methodId.returnType.isJsBasic || methodId.returnType is JsMultipleClassId)) res += methodId.returnType.name
         return res
     }
-
-    private fun runJs(scriptText: String, workDir: String): String {
-        val (reader, errorReader) = JsCmdExec.runCommand(
-            dir = workDir,
-            shouldWait = true,
-            timeout = settings.timeout,
-            cmd = arrayOf(settings.pathToNode, "-e", "\"$scriptText\"")
-        )
-        return errorReader.readText().ifEmpty { reader.readText() }
-    }
-
-    private fun makeStringForRunJs(
-        fuzzedValue: List<FuzzedValue>,
-        method: JsMethodId,
-        containingClass: TruffleString?,
-        importText: String,
-    ): String {
-        val callString = makeCallFunctionString(fuzzedValue, method, containingClass)
-        val prefix = functionCallResultAnchor
-        val temp = "console.log(`$prefix \\\"\${res}\\\"`)"
-        return "const $fileUnderTestAliases = require(\\\"./$importText\\\"); " +
-                "let prefix = \\\"$prefix\\\"; " +
-                "let res = $callString; " +
-                "if (typeof res == \\\"string\\\") {$temp} else console.log(prefix, res)"
-    }
-
-    private fun makeCallFunctionString(
-        fuzzedValue: List<FuzzedValue>,
-        method: JsMethodId,
-        containingClass: TruffleString?
-    ): String {
-        val initClass = containingClass?.let {
-            if (!method.isStatic) {
-                "new $fileUnderTestAliases.${it}()."
-            } else "$fileUnderTestAliases.$it."
-        } ?: "$fileUnderTestAliases."
-        var callString = "$initClass${method.name}"
-        callString += fuzzedValue.joinToString(
-            prefix = "(",
-            postfix = ")",
-        ) { value -> value.model.toCallString() }
-        return callString
-    }
-
-    private fun Any.quoteWrapIfNecessary(): String =
-        when (this) {
-            is String -> "\"$this\""
-            else -> "$this"
-        }
-
-    private fun UtAssembleModel.toParamString(): String =
-        with(this) {
-            val callConstructorString = "new $fileUnderTestAliases.${classId.name}"
-            val paramsString = instantiationCall.params.joinToString(
-                prefix = "(",
-                postfix = ")",
-            ) {
-                (it as JsPrimitiveModel).value.quoteWrapIfNecessary()
-            }
-            return callConstructorString + paramsString
-        }
-
-    private fun UtModel.toCallString(): String =
-        when (this) {
-            is UtAssembleModel -> this.toParamString()
-            else -> {
-                (this as JsPrimitiveModel).value.quoteWrapIfNecessary()
-            }
-        }
-
 
     private fun analyzeCoverage(coverageList: List<Set<Int>>): List<Int> {
         val allCoveredBranches = mutableSetOf<Int>()
--- utbot-js/src/main/kotlin/settings/JsDynamicSettings.kt
+++ utbot-js/src/main/kotlin/settings/JsDynamicSettings.kt
@@ -1,8 +1,11 @@
 package settings
 
+import service.CoverageMode
+
 data class JsDynamicSettings(
     val pathToNode: String = "node",
     val pathToNYC: String = "nyc",
     val pathToNPM: String = "npm",
     val timeout: Long = 15L,
+    val coverageMode: CoverageMode = CoverageMode.FAST
 )

2269f60b25ffac77dd3a053fde01c927a15919b5
Egor Kulikov <egor.k.kulikov@gmail.com>
2022-10-17 11:45:06 +0300

    Introduce utbot-android-studio module

---
+++ utbot-android-studio/src/main/kotlin/org/androidstudio/plugin/util/UtAndroidGradleJavaProjectModelModifier.kt
@@ -0,0 +1,140 @@
+package org.androidstudio.plugin.util
+
+import com.android.tools.idea.gradle.AndroidGradleJavaProjectModelModifier
+import com.android.tools.idea.gradle.dsl.api.GradleBuildModel
+import com.android.tools.idea.gradle.dsl.api.dependencies.ArtifactDependencySpec
+import com.android.tools.idea.gradle.dsl.api.dependencies.CommonConfigurationNames
+import com.android.tools.idea.gradle.project.sync.GradleSyncInvoker
+import com.android.tools.idea.gradle.project.sync.GradleSyncListener
+import com.android.tools.idea.gradle.project.sync.idea.GradleSyncExecutor
+import com.android.tools.idea.gradle.util.GradleUtil
+import com.android.tools.idea.project.AndroidProjectInfo
+import com.android.tools.idea.projectsystem.TestArtifactSearchScopes
+import com.google.wireless.android.sdk.stats.GradleSyncStats
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.command.undo.BasicUndoableAction
+import com.intellij.openapi.command.undo.UndoManager
+import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.DependencyScope
+import com.intellij.openapi.roots.ExternalLibraryDescriptor
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.containers.ContainerUtil
+import org.jetbrains.concurrency.AsyncPromise
+import org.jetbrains.concurrency.Promise
+
+class UtAndroidGradleJavaProjectModelModifier : AndroidGradleJavaProjectModelModifier() {
+    override fun addExternalLibraryDependency(
+        modules: Collection<Module?>,
+        descriptor: ExternalLibraryDescriptor,
+        scope: DependencyScope
+    ): Promise<Void?>? {
+        val module = ContainerUtil.getFirstItem(modules) ?: return null
+
+        if (!isAndroidGradleProject(module.project)) {
+            return null
+        }
+
+        val dependencySpec = ArtifactDependencySpec.create(descriptor.libraryArtifactId, descriptor.libraryGroupId, descriptor.preferredVersion)
+        return addExternalLibraryDependency(module, dependencySpec, scope)
+
+    }
+
+    // returns single external project path if it is the same for all the modules, or null
+    private fun getSingleExternalProjectPathOrNull(modules: Collection<Module>): String? {
+        var projectPath: String? = null
+        for (module in modules) {
+            val rootProjectPathForModule = getSingleExternalProjectPathOrNull(module)
+                ?: return null
+            if (projectPath == null) {
+                projectPath = rootProjectPathForModule
+            } else if (projectPath != rootProjectPathForModule) {
+                return null
+            }
+        }
+        return projectPath
+    }
+
+    private fun getSingleExternalProjectPathOrNull(module: Module): String? =
+        ExternalSystemModulePropertyManager.getInstance(module).getRootProjectPath()
+
+    private fun addExternalLibraryDependency(
+        module: Module,
+        dependencySpec: ArtifactDependencySpec,
+        scope: DependencyScope,
+    ): Promise<Void?>? {
+        val project = module.project
+        val openedFile = FileEditorManagerEx.getInstanceEx(project).currentFile
+        val buildModelsToUpdate: MutableList<GradleBuildModel> = ArrayList()
+
+        val buildModel = GradleBuildModel.get(module) ?: return null
+        val configurationName = getConfigurationName(module, scope, openedFile)
+        val dependencies = buildModel.dependencies()
+        dependencies.addArtifact(configurationName, dependencySpec)
+        buildModelsToUpdate.add(buildModel)
+
+        WriteCommandAction.writeCommandAction(project).withName("Add Gradle Library Dependency").run<RuntimeException> {
+            buildModelsToUpdate.forEach { buildModel -> buildModel.applyChanges() }
+            registerUndoAction(project)
+        }
+
+        return doAndroidGradleSync(project, GradleSyncStats.Trigger.TRIGGER_MODIFIER_ADD_LIBRARY_DEPENDENCY)
+    }
+
+    private fun getConfigurationName(module: Module, scope: DependencyScope, openedFile: VirtualFile?): String =
+        GradleUtil.mapConfigurationName(
+            getLegacyConfigurationName(module, scope, openedFile),
+            GradleUtil.getAndroidGradleModelVersionInUse(module),
+            false
+        )
+
+    private fun getLegacyConfigurationName(
+        module: Module,
+        scope: DependencyScope,
+        openedFile: VirtualFile?
+    ): String {
+        if (!scope.isForProductionCompile) {
+            val testScopes = TestArtifactSearchScopes.getInstance(module)
+            if (testScopes != null && openedFile != null) {
+                return if (testScopes.isAndroidTestSource(openedFile)) CommonConfigurationNames.ANDROID_TEST_COMPILE else CommonConfigurationNames.TEST_COMPILE
+            }
+        }
+        return CommonConfigurationNames.COMPILE
+    }
+
+    private fun registerUndoAction(project: Project) {
+        UndoManager.getInstance(project).undoableActionPerformed(object : BasicUndoableAction() {
+
+            override fun undo() {
+                doAndroidGradleSync(project, GradleSyncStats.Trigger.TRIGGER_MODIFIER_ACTION_UNDONE)
+
+            }
+
+            override fun redo() {
+                doAndroidGradleSync(project, GradleSyncStats.Trigger.TRIGGER_MODIFIER_ACTION_REDONE)
+            }
+        })
+    }
+
+    private fun isAndroidGradleProject(project: Project): Boolean =
+        AndroidProjectInfo.getInstance(project).requiresAndroidModel()
+
+    private fun doAndroidGradleSync(project: Project, trigger: GradleSyncStats.Trigger): AsyncPromise<Void?> {
+        val promise = AsyncPromise<Void?>()
+        val request = GradleSyncInvoker.Request(trigger)
+        val listener = object : GradleSyncListener {
+            override fun syncSucceeded(project: Project) {
+                promise.setResult(null)
+            }
+
+            override fun syncFailed(project: Project, errorMessage: String) {
+                promise.setError(errorMessage)
+            }
+        }
+        GradleSyncExecutor(project).sync(request, listener)
+
+        return promise
+    }
+}

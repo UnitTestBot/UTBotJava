319bc54461f7a7fe9ea15e1946bfbea87af623b5
Vladislav Kasimov <vladislavkasimov@gmail.com>
2022-10-17 13:16:24 +0300

    Added nyc path field into dialog window
    * Now only nyc from path field is used
    * Added validation for nyc

--- utbot-intellij-js/src/main/kotlin/org/utbot/intellij/plugin/language/js/NycSourceFileChooser.kt
+++ utbot-intellij-js/src/main/kotlin/org/utbot/intellij/plugin/language/js/NycSourceFileChooser.kt
@@ -3,6 +3,8 @@ package org.utbot.intellij.plugin.language.js
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.ui.TextBrowseFolderListener
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.ui.ValidationInfo
+import org.utbot.common.PathUtil.replaceSeparator
 import settings.JsDynamicSettings
 
 
@@ -21,6 +23,13 @@ class NycSourceFileChooser(val model: JsTestsModel) : TextFieldWithBrowseButton(
         addBrowseFolderListener(
             TextBrowseFolderListener(descriptor, model.project)
         )
-        text = getFrameworkLibraryPath(JsDynamicSettings().pathToNYC, model) ?: "Nyc does not found"
+        text = replaceSeparator(getFrameworkLibraryPath(JsDynamicSettings().pathToNYC, model) ?: "Nyc does not found")
+    }
+
+    fun validateNyc(): ValidationInfo? {
+        return if (text.endsWith("npm/nyc"))
+            null
+        else
+            ValidationInfo("Nyc executable file was not found in the specified directory", this)
     }
 }

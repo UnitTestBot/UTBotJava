7e16b6671a3505a8079711f6051dd2beb776bb89
Sergey Loktev <loktevsergay.2003@gmail.com>
2022-11-07 10:13:13 +0300

    Removed unnecessary JS file parsing

--- utbot-js/src/main/kotlin/service/ServiceContext.kt
+++ utbot-js/src/main/kotlin/service/ServiceContext.kt
@@ -1,12 +1,12 @@
 package service
 
+import com.oracle.js.parser.ir.FunctionNode
 import settings.JsDynamicSettings
 
 data class ServiceContext(
     val utbotDir: String,
     val projectPath: String,
     val filePathToInference: String,
-    val trimmedFileText: String,
-    val fileText: String? = null,
+    val parsedFile: FunctionNode,
     val settings: JsDynamicSettings,
 )

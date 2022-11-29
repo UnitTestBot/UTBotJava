24407d59485937e69ab3d4b1746917c20b7bc131
Ekaterina Tochilina <katerina_t_n@mail.ru>
2022-11-29 11:14:05 +0300

    Build initialState in BaselineAlgorithm

--- utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/TypeInferenceAPI.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/TypeInferenceAPI.kt
@@ -1,44 +1,41 @@
 package org.utbot.python.newtyping.inference
 
-import org.utbot.python.newtyping.PythonTypeWrapperForEqualityCheck
 import org.utbot.python.newtyping.ast.visitor.hints.HintCollectorResult
 import org.utbot.python.newtyping.general.Type
 
-open class TypeInferenceEdge(
-    val from: TypeInferenceNode,
-    val to: TypeInferenceNode,
-    val dependency: (Type) -> (TypeRestrictionSet) -> TypeRestrictionSet
-)
-
-abstract class TypeRestriction
-
-abstract class TypeRestrictionSet
-
-abstract class TypeInferenceNode(
-    val ingoingEdges: Set<TypeInferenceEdge>,
-    val outgoingEdges: Set<TypeInferenceEdge>,
-    val restrictions: TypeRestrictionSet
-)
-
-abstract class TypeInferenceState {
-    abstract val currentFunctionSignature: Type
-    abstract val nodes: Set<TypeInferenceNode>
+abstract class TypeInferenceAlgorithm {
+    abstract fun run(hintCollectorResult: HintCollectorResult): Sequence<Type>
 }
 
-abstract class TypeInferenceAlgorithm<NODE : TypeInferenceNode, STATE : TypeInferenceState> {
-    fun run(initialSignature: Type, hintCollectorResult: HintCollectorResult): Sequence<Type> {
-        val initialState = getInitialState(hintCollectorResult)
-        val states = setOf(initialState)
-        val foundSignatures: MutableSet<PythonTypeWrapperForEqualityCheck> =
-            mutableSetOf(PythonTypeWrapperForEqualityCheck(initialSignature))
-        TODO()
+interface TypeInferenceNode {
+    val partialType: Type
+    val ingoingEdges: Collection<TypeInferenceEdge>
+    val outgoingEdges: Collection<TypeInferenceEdge>
+}
+
+interface TypeInferenceEdge {
+    val from: TypeInferenceNode
+    val to: TypeInferenceNode
+}
+
+interface TypeInferenceEdgeWithValue: TypeInferenceEdge {
+    val annotationParameterId: Int
+}
+
+interface TypeInferenceEdgeWithBound: TypeInferenceEdge {
+    val boundType: BoundType
+    val dependency: (Type) -> List<Type>
+    enum class BoundType {
+        Lower,
+        Upper
     }
-    abstract fun getInitialState(hintCollectorResult: HintCollectorResult): STATE
-    abstract fun chooseState(states: Set<STATE>): STATE
-    abstract fun chooseNode(state: STATE): NODE
-    abstract fun expandNode(node: NODE): NodeExpansionResult<NODE>
-    class NodeExpansionResult<NODE: TypeInferenceNode>(
-        val type: Type,
-        val newNodes: Set<NODE>
-    )
+}
+
+fun addEdge(edge: TypeInferenceEdge) {
+    val fromEdgeStorage = edge.from.outgoingEdges
+    val toEdgeStorage = edge.to.ingoingEdges
+    if (fromEdgeStorage is MutableCollection)
+        fromEdgeStorage.add(edge)
+    if (toEdgeStorage is MutableCollection)
+        toEdgeStorage.add(edge)
 }

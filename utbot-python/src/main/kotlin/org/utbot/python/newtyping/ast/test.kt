0a96b390473c561627fb98690ba5fa61425bee46
Ekaterina Tochilina <katerina_t_n@mail.ru>
2022-12-02 14:22:20 +0300

    First version of BaselineAlgorithm

--- utbot-python/src/main/kotlin/org/utbot/python/newtyping/ast/test.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/newtyping/ast/test.kt
@@ -3,17 +3,15 @@ package org.utbot.python.newtyping.ast
 import org.parsers.python.PythonParser
 import org.parsers.python.ast.Block
 import org.parsers.python.ast.FunctionDefinition
-import org.utbot.python.newtyping.PythonCallableTypeDescription
+import org.utbot.python.newtyping.*
 import org.utbot.python.newtyping.ast.visitor.Visitor
 import org.utbot.python.newtyping.ast.visitor.hints.HintCollector
-import org.utbot.python.newtyping.createPythonCallableType
 import org.utbot.python.newtyping.general.FunctionTypeCreator
 import org.utbot.python.newtyping.inference.baseline.BaselineAlgorithm
-import org.utbot.python.newtyping.pythonAnyType
-import org.utbot.python.newtyping.readMypyAnnotationStorage
 
 fun main() {
-    HintCollector::class.java.getResource("/annotation_sample.json")?.let { readMypyAnnotationStorage(it.readText()) }
+    val mypyStorage = HintCollector::class.java.getResource("/annotation_sample.json")?.let { readMypyAnnotationStorage(it.readText()) }
+    val storage = PythonTypeStorage.get(mypyStorage!!)
 
     val content = """
         import collections
@@ -47,9 +45,12 @@ fun main() {
             isStaticMethod = false
         ) {
             FunctionTypeCreator.InitializationData(listOf(pythonAnyType, pythonAnyType), pythonAnyType)
-        }
+        },
+        storage
     )
     val visitor = Visitor(listOf(collector))
     visitor.visit(functionBlock)
-    BaselineAlgorithm.run(collector.result)
+    BaselineAlgorithm(storage).run(collector.result).forEach {
+        println(it.pythonTypeRepresentation())
+    }
 }
--- utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/baseline/BaselineAlgorithm.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/baseline/BaselineAlgorithm.kt
@@ -2,18 +2,82 @@ package org.utbot.python.newtyping.inference.baseline
 
 import org.utbot.python.newtyping.*
 import org.utbot.python.newtyping.ast.visitor.hints.*
+import org.utbot.python.newtyping.general.DefaultSubstitutionProvider
 import org.utbot.python.newtyping.general.Type
 import org.utbot.python.newtyping.inference.*
 
-object BaselineAlgorithm: TypeInferenceAlgorithm() {
-    override fun run(hintCollectorResult: HintCollectorResult): Sequence<Type> {
-        val initialState = getInitialState(hintCollectorResult)
-        return emptySequence()  // TODO
+class BaselineAlgorithm(val storage: PythonTypeStorage): TypeInferenceAlgorithm() {
+    override fun run(hintCollectorResult: HintCollectorResult): Sequence<Type> = sequence {
+        var state = getInitialState(hintCollectorResult)
+        var nodeForExpansion = chooseNodeForExpansion(state)
+        while (nodeForExpansion != null) {
+            state = expandNode(state, nodeForExpansion)
+            nodeForExpansion = chooseNodeForExpansion(state)
+            yield(state.signature)
+        }
+    }
+
+    // TODO: create rating, add new nodes for generics
+    private fun chooseTypeForAny(node: AnyTypeNode): Type {
+        val scores: Map<Type, Int> = storage.allTypes.associateWith { typeFromStorage ->
+            val type = DefaultSubstitutionProvider.substituteAll(
+                typeFromStorage,
+                List(typeFromStorage.parameters.size) { pythonAnyType }
+            )
+            node.lowerBounds.fold(0) { acc, constraint ->
+                acc + (if (PythonSubtypeChecker.checkIfRightIsSubtypeOfLeft(type, constraint, storage)) 1 else 0)
+            }
+            + node.upperBounds.fold(0) { acc, constraint ->
+                acc + (if (PythonSubtypeChecker.checkIfRightIsSubtypeOfLeft(constraint, type, storage)) 1 else 0)
+            }
+        }
+        return scores.maxBy { it.value }.key
+    }
+
+    // TODO: something smarter?
+    private fun chooseNodeForExpansion(state: BaselineAlgorithmState): AnyTypeNode? {
+        return state.nodes.find { it is AnyTypeNode }?.let { it as AnyTypeNode }
+    }
+
+    private fun expandNode(state: BaselineAlgorithmState, node: AnyTypeNode): BaselineAlgorithmState {
+        assert(state.nodes.contains(node))
+        val newType = chooseTypeForAny(node)
+        val newNodeMap = expansionDFS(node, newType).toMutableMap()
+        val newNodes: MutableSet<BaselineAlgorithmNode> = mutableSetOf()
+        state.nodes.forEach { cur -> newNodeMap[cur] = newNodeMap[cur] ?: cur.copy() }
+        state.nodes.forEach { cur ->
+            val new = newNodeMap[cur]!!
+            newNodes.add(new)
+            cur.outgoingEdges.forEach { edge ->
+                val newEdge = BaselineAlgorithmEdge(
+                    from = new,
+                    to = newNodeMap[edge.to]!!,
+                    annotationParameterId = edge.annotationParameterId
+                )
+                addEdge(newEdge)
+            }
+        }
+        return BaselineAlgorithmState(newNodes)
+    }
+
+    private fun expansionDFS(
+        oldNode: BaselineAlgorithmNode,
+        newType: Type,
+    ): Map<BaselineAlgorithmNode, BaselineAlgorithmNode> {
+        val newNode = PartialTypeNode(newType, oldNode.isRoot)
+        val result: MutableMap<BaselineAlgorithmNode, BaselineAlgorithmNode> = mutableMapOf(oldNode to newNode)
+        oldNode.outgoingEdges.forEach { edge ->
+            val params = edge.to.partialType.pythonAnnotationParameters().toMutableList()
+            params[edge.annotationParameterId] = newType
+            val type = edge.to.partialType.pythonDescription().createTypeWithNewAnnotationParameters(edge.to.partialType, params)
+            result += expansionDFS(edge.to, type)
+        }
+        return result
     }
 
     private fun getInitialState(hintCollectorResult: HintCollectorResult): BaselineAlgorithmState {
         val signatureDescription = hintCollectorResult.initialSignature.pythonDescription() as PythonCallableTypeDescription
-        val root = PartialTypeNode(hintCollectorResult.initialSignature)
+        val root = PartialTypeNode(hintCollectorResult.initialSignature, true)
         val allNodes: MutableSet<BaselineAlgorithmNode> = mutableSetOf(root)
         val argumentRootNodes = signatureDescription.argumentNames.map { hintCollectorResult.parameterToNode[it]!! }
         argumentRootNodes.forEachIndexed { index, node ->
@@ -35,7 +99,7 @@ object BaselineAlgorithm: TypeInferenceAlgorithm() {
             )
             addEdge(edge)
         }
-        return BaselineAlgorithmState(allNodes, root)
+        return BaselineAlgorithmState(allNodes)
     }
 
     private fun visitIdentificationNodes(
@@ -55,7 +119,7 @@ object BaselineAlgorithm: TypeInferenceAlgorithm() {
             return DecompositionResult(root, setOf(root))
         }
         val newNodes: MutableSet<BaselineAlgorithmNode> = mutableSetOf()
-        val root = PartialTypeNode(partialType)
+        val root = PartialTypeNode(partialType, false)
         newNodes.add(root)
         val children = partialType.pythonAnnotationParameters()
         val constraints: Map<Int, MutableList<TypeConstraint>> =
@@ -114,10 +178,10 @@ object BaselineAlgorithm: TypeInferenceAlgorithm() {
     )
 }
 
-class BaselineAlgorithmState(
-    val nodes: Set<BaselineAlgorithmNode>,
-    val signature: PartialTypeNode
-)
+class BaselineAlgorithmState(val nodes: Set<BaselineAlgorithmNode>) {
+    val signature: Type
+        get() = nodes.find { it.isRoot }!!.partialType
+}
 
 class BaselineAlgorithmEdge(
     override val from: BaselineAlgorithmNode,
@@ -125,11 +189,15 @@ class BaselineAlgorithmEdge(
     override val annotationParameterId: Int
 ): TypeInferenceEdgeWithValue
 
-sealed class BaselineAlgorithmNode: TypeInferenceNode {
+sealed class BaselineAlgorithmNode(val isRoot: Boolean): TypeInferenceNode {
     override val ingoingEdges: MutableList<BaselineAlgorithmEdge> = mutableListOf()
     override val outgoingEdges: MutableList<BaselineAlgorithmEdge> = mutableListOf()
+    abstract fun copy(): BaselineAlgorithmNode
 }
-class PartialTypeNode(override val partialType: Type): BaselineAlgorithmNode()
-class AnyTypeNode(val lowerBounds: List<Type>, val upperBounds: List<Type>): BaselineAlgorithmNode() {
+class PartialTypeNode(override val partialType: Type, isRoot: Boolean): BaselineAlgorithmNode(isRoot) {
+    override fun copy(): BaselineAlgorithmNode = PartialTypeNode(partialType, isRoot)
+}
+class AnyTypeNode(val lowerBounds: List<Type>, val upperBounds: List<Type>): BaselineAlgorithmNode(false) {
     override val partialType: Type = pythonAnyType
+    override fun copy(): BaselineAlgorithmNode = AnyTypeNode(lowerBounds, upperBounds)
 }

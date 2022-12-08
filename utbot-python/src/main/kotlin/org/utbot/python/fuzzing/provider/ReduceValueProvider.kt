103a7b637bf17a8bf65cdfd2665f1e11ff8dfd93
Vyacheslav Tamarin <vyacheslav.tamarin@yandex.ru>
2022-12-08 12:42:28 +0300

    Add reduce value provider

---
+++ utbot-python/src/main/kotlin/org/utbot/python/fuzzing/provider/ReduceValueProvider.kt
@@ -0,0 +1,42 @@
+package org.utbot.python.fuzzing.provider
+
+import org.utbot.fuzzer.IdGenerator
+import org.utbot.fuzzing.Routine
+import org.utbot.fuzzing.Seed
+import org.utbot.fuzzing.ValueProvider
+import org.utbot.python.framework.api.python.PythonClassId
+import org.utbot.python.framework.api.python.PythonTree
+import org.utbot.python.framework.api.python.PythonTreeModel
+import org.utbot.python.fuzzing.PythonMethodDescription
+import org.utbot.python.newtyping.PythonConcreteCompositeTypeDescription
+import org.utbot.python.newtyping.general.FunctionTypeCreator
+import org.utbot.python.newtyping.general.Type
+import org.utbot.python.newtyping.getPythonAttributes
+
+class ReduceValueProvider(
+    private val idGenerator: IdGenerator<Long>
+) : ValueProvider<Type, PythonTreeModel, PythonMethodDescription> {
+    override fun accept(type: Type): Boolean {
+        val hasInit = type.getPythonAttributes().any { it.name == "__init__" }
+        return type.meta is PythonConcreteCompositeTypeDescription && hasInit
+    }
+
+    override fun generate(description: PythonMethodDescription, type: Type) = sequence {
+        val meta = type.meta as PythonConcreteCompositeTypeDescription
+        val arguments = (type.getPythonAttributes().first { it.name == "__init__" }.type as FunctionTypeCreator.Original).arguments
+        yield(Seed.Recursive(
+            construct = Routine.Create(arguments) { v ->
+                PythonTreeModel(
+                    PythonTree.ReduceNode(
+                        idGenerator.createId(),
+                        PythonClassId(meta.name.toString()),
+                        PythonClassId(meta.name.toString()),
+                        v.map { it.tree },
+                    ),
+                    PythonClassId(meta.name.toString())
+                )
+            },
+            empty = Routine.Empty { PythonTreeModel(PythonTree.fromNone(), PythonClassId(meta.name.toString())) }
+        ))
+    }
+}

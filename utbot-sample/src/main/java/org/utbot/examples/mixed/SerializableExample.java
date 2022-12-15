98dc1dd3d7fdd5cb2e9a202369a63138f801a248
Alexey Menshutin <alex.menshutin99@gmail.com>
2022-12-15 19:19:52 +0800

    Add interfaces of superclasses in ancestors set

---
+++ utbot-sample/src/main/java/org/utbot/examples/mixed/SerializableExample.java
@@ -0,0 +1,13 @@
+package org.utbot.examples.mixed;
+
+import java.io.File;
+
+public class SerializableExample {
+    public void example() {
+        join("string", File.separator, System.currentTimeMillis());
+    }
+
+    public static <T> String join(T... elements) {
+        return null;
+    }
+}

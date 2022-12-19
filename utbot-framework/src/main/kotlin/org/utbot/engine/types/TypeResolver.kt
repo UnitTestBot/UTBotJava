4eb9a87669c1bd1a74d8848fd019c1e0c5c2b29a
Yury Kamenev <yurkam447@gmail.com>
2022-12-19 14:21:45 +0800

    Added symbolic processing of `newInstance` reflection call (#1540)
    (cherry picked from commit ac31b7f2cb416f3126bd821777887e155e509423)

--- utbot-framework/src/main/kotlin/org/utbot/engine/types/TypeResolver.kt
+++ utbot-framework/src/main/kotlin/org/utbot/engine/types/TypeResolver.kt
@@ -39,6 +39,7 @@ import soot.NullType
 import soot.PrimType
 import soot.RefType
 import soot.Scene
+import soot.SootClass
 import soot.SootField
 import soot.Type
 import soot.VoidType
@@ -344,12 +345,17 @@ internal val CLASS_REF_NUM_DIMENSIONS_DESCRIPTOR: MemoryChunkDescriptor
         IntType.v()
     )
 
+internal val CLASS_REF_SOOT_CLASS: SootClass
+    get() = Scene.v().getSootClass(CLASS_REF_CLASSNAME)
+
 internal val OBJECT_TYPE: RefType
     get() = Scene.v().getSootClass(Object::class.java.canonicalName).type
 internal val STRING_TYPE: RefType
     get() = Scene.v().getSootClass(String::class.java.canonicalName).type
 internal val CLASS_REF_TYPE: RefType
-    get() = Scene.v().getSootClass(CLASS_REF_CLASSNAME).type
+    get() = CLASS_REF_SOOT_CLASS.type
+
+internal val NEW_INSTANCE_SIGNATURE: String = CLASS_REF_SOOT_CLASS.getMethodByName("newInstance").subSignature
 
 internal val HASHCODE_SIGNATURE: String =
     Scene.v()

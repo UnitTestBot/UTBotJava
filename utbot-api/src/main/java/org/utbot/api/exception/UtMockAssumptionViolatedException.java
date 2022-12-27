6cb8e93bb9c33a5830cdfea2eabc139681561b56
Nikita Stroganov <stroganov.n.36@gmail.com>
2022-12-27 17:24:35 +0300

    Add custom serializer for utmock exception

--- utbot-api/src/main/java/org/utbot/api/exception/UtMockAssumptionViolatedException.java
+++ utbot-api/src/main/java/org/utbot/api/exception/UtMockAssumptionViolatedException.java
@@ -1,11 +1,8 @@
 package org.utbot.api.exception;
 
 public class UtMockAssumptionViolatedException extends RuntimeException {
-
-    public static final String errorMessage = "UtMock assumption violated";
-
     @Override
     public String getMessage() {
-        return errorMessage;
+        return "UtMock assumption violated";
     }
 }

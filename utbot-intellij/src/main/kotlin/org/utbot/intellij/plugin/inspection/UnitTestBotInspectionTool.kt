4643f8d9cc10d8bf3df30ea577d99ef9b3682930
Nikita Stroganov <stroganov.n.36@gmail.com>
2022-12-27 15:43:45 +0300

    Fix non-physical psi element in inspections

--- utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/inspection/UnitTestBotInspectionTool.kt
+++ utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/inspection/UnitTestBotInspectionTool.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
+import org.jetbrains.kotlin.idea.core.util.toPsiFile
 import org.jetbrains.kotlin.idea.search.allScope
 import org.utbot.sarif.*
 import java.nio.file.Path
@@ -59,7 +60,8 @@ class UnitTestBotInspectionTool : GlobalSimpleInspectionTool() {
             val errorPsiFile = srcFileLogicalLocation?.fullyQualifiedName?.let { errorClassFqn ->
                 val psiFacade = JavaPsiFacade.getInstance(srcPsiFile.project)
                 val psiClass = psiFacade.findClass(errorClassFqn, srcPsiFile.project.allScope())
-                psiClass?.containingFile
+                // We can't just return `psiClass?.containingFile` because it may be non-physical
+                psiClass?.containingFile?.virtualFile?.toPsiFile(srcPsiFile.project)
             } ?: srcPsiFile
             val errorRegion = srcFilePhysicalLocation.region
             val errorTextRange = getTextRange(problemsHolder.project, errorPsiFile, errorRegion)

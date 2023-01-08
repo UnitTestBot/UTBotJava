d56c120bee953e9587cb1200f4dae21bc042a7c8
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-01-09 00:54:14 +0300

    Faster version of StateExpansion

--- utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/baseline/StateExpansion.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/baseline/StateExpansion.kt
@@ -5,37 +5,33 @@ import org.utbot.python.newtyping.general.Type
 import org.utbot.python.newtyping.inference.addEdge
 import org.utbot.python.newtyping.pythonAnnotationParameters
 import org.utbot.python.newtyping.pythonDescription
+import java.util.LinkedList
+import java.util.Queue
 
 fun expandState(state: BaselineAlgorithmState, typeStorage: PythonTypeStorage): BaselineAlgorithmState? {
     if (state.anyNodes.isEmpty())
         return null
     val types = state.candidateGraph.getNext() ?: return null
-    val map: MutableMap<AnyTypeNode, AnyTypeNode> = mutableMapOf()
-    return (state.anyNodes zip types).fold(state) { newState, (anyTypeNode, newType) ->
-        var mappedNode = anyTypeNode
-        while (map[mappedNode] != null)
-            mappedNode = map[mappedNode]!!
-        val (newNewState, newMap) = expandNode(newState, mappedNode, newType, state.generalRating, typeStorage)
-        newMap.forEach { map[it.key] = it.value }
-        newNewState
-    }
+    val substitution = (state.anyNodes zip types).associate { it }
+    return expandNodes(state, substitution, state.generalRating, typeStorage)
 }
 
-private fun expandNode(
+private fun expandNodes(
     state: BaselineAlgorithmState,
-    node: AnyTypeNode,
-    newType: Type,
+    substitution: Map<AnyTypeNode, Type>,
     generalRating: TypeRating,
     storage: PythonTypeStorage
-): Pair<BaselineAlgorithmState, Map<AnyTypeNode, AnyTypeNode>> {
-    assert(state.nodes.contains(node))
-    val (newNodeForAny, additionalNodes) = decompose(newType, node.lowerBounds, node.upperBounds, node.nestedLevel)
-    val newNodeMap = expansionDFS(node, newType, newNodeForAny).toMutableMap()
-    val newNodes: MutableSet<BaselineAlgorithmNode> = mutableSetOf()
+): BaselineAlgorithmState {
+    val (newAnyNodeMap, allNewNodes) = substitution.entries.fold(
+        Pair(emptyMap<AnyTypeNode, BaselineAlgorithmNode>(), emptySet<BaselineAlgorithmNode>())
+    ) { (map, allNodeSet), (node, newType) ->
+        val (newNodeForAny, additionalNodes) = decompose(newType, node.lowerBounds, node.upperBounds, node.nestedLevel)
+        Pair(map + (node to newNodeForAny), allNodeSet + additionalNodes)
+    }
+    val newNodeMap = expansionBFS(substitution, newAnyNodeMap).toMutableMap()
     state.nodes.forEach { cur -> newNodeMap[cur] = newNodeMap[cur] ?: cur.copy() }
     state.nodes.forEach { cur ->
         val new = newNodeMap[cur]!!
-        newNodes.add(new)
         cur.outgoingEdges.forEach { edge ->
             val newEdge = BaselineAlgorithmEdge(
                 from = new,
@@ -45,30 +41,42 @@ private fun expandNode(
             addEdge(newEdge)
         }
     }
-    return Pair(
-        BaselineAlgorithmState(newNodes + additionalNodes, generalRating, storage),
-        newNodeMap.mapNotNull {
-            if (it.key !is AnyTypeNode || it.value !is AnyTypeNode) null else Pair(
-                it.key as AnyTypeNode,
-                it.value as AnyTypeNode
-            )
-        }.associate { it }
-    )
+    return BaselineAlgorithmState(newNodeMap.values.toSet() + allNewNodes, generalRating, storage)
 }
 
-private fun expansionDFS(
-    oldNode: BaselineAlgorithmNode,
-    newType: Type,
-    newNodeOpt: BaselineAlgorithmNode? = null
+private fun expansionBFS(
+    substitution: Map<AnyTypeNode, Type>,
+    nodeMap: Map<AnyTypeNode, BaselineAlgorithmNode>
 ): Map<BaselineAlgorithmNode, BaselineAlgorithmNode> {
-    val newNode = newNodeOpt ?: PartialTypeNode(newType, oldNode.isRoot)
-    val result: MutableMap<BaselineAlgorithmNode, BaselineAlgorithmNode> = mutableMapOf(oldNode to newNode)
-    oldNode.outgoingEdges.forEach { edge ->
-        val params = edge.to.partialType.pythonAnnotationParameters().toMutableList()
-        params[edge.annotationParameterId] = newType
-        val type = edge.to.partialType.pythonDescription()
-            .createTypeWithNewAnnotationParameters(edge.to.partialType, params)
-        result += expansionDFS(edge.to, type)
+    val queue: Queue<Pair<BaselineAlgorithmNode, Int>> = LinkedList(substitution.keys.map { Pair(it, 0) })
+    val newParams: MutableMap<BaselineAlgorithmNode, MutableList<Type>> = mutableMapOf()
+    val newNodes: MutableMap<BaselineAlgorithmNode, BaselineAlgorithmNode> = nodeMap.toMutableMap()
+    val lastModified: MutableMap<BaselineAlgorithmNode, Int> = mutableMapOf()
+    var timer = 0
+    while (!queue.isEmpty()) {
+        timer++
+        val (oldNode, putTime) = queue.remove()
+        if ((lastModified[oldNode] ?: 0) != putTime)
+            continue
+        if (!newNodes.keys.contains(oldNode)) {
+            val oldType = oldNode.partialType
+            val newType = if (substitution.keys.contains(oldNode))
+                substitution[oldNode]!!
+            else
+                oldType.pythonDescription().createTypeWithNewAnnotationParameters(
+                    oldType,
+                    newParams[oldNode] ?: oldType.pythonAnnotationParameters()
+                )
+            newNodes[oldNode] = PartialTypeNode(newType, oldNode.isRoot)
+        }
+        val newType = newNodes[oldNode]!!.partialType
+        oldNode.outgoingEdges.forEach { edge ->
+            val params = newParams[edge.to] ?: edge.to.partialType.pythonAnnotationParameters().toMutableList()
+            params[edge.annotationParameterId] = newType
+            newParams[edge.to] = params
+            lastModified[edge.to] = timer
+            queue.add(Pair(edge.to, timer))
+        }
     }
-    return result
+    return newNodes
 }

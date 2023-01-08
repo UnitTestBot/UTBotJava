57af5b3f90bd0cf2e38da19b9164363aeb11e138
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-01-09 00:54:14 +0300

    Faster version of StateExpansion

--- utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/test.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/test.kt
@@ -1,9 +1,13 @@
 package org.utbot.python.newtyping.inference
 
+import org.utbot.python.newtyping.pythonTypeRepresentation
+
 fun main() {
     TypeInferenceProcessor(
         "python3",
         "/home/tochilinak/Documents/projects/utbot/PythonRobotics/PathPlanning/DubinsPath/dubins_path_planner.py",
         "_interpolate"
-    ).inferTypes(cancel = { false }).forEach {  }
+    ).inferTypes(cancel = { false }).forEach {
+        println(it.pythonTypeRepresentation())
+    }
 }

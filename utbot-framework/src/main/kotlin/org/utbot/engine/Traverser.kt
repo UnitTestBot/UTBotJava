8818c523174463a7a7857ca7638c4c13ab9223b7
Alexey Menshutin <alex.menshutin99@gmail.com>
2023-01-09 16:47:31 +0800

    Remove error from generics processing

--- utbot-framework/src/main/kotlin/org/utbot/engine/Traverser.kt
+++ utbot-framework/src/main/kotlin/org/utbot/engine/Traverser.kt
@@ -1111,7 +1111,8 @@ class Traverser(
 
                     if (allTypes.any { it is GenericArrayType }) {
                         val errorTypes = allTypes.filterIsInstance<GenericArrayType>()
-                        TODO("we do not support GenericArrayTypeImpl yet, and $errorTypes found. SAT-1446")
+                        logger.warn { "we do not support GenericArrayTypeImpl yet, and $errorTypes found. SAT-1446" }
+                        return
                     }
 
                     val upperBoundsTypes = typeResolver.intersectInheritors(upperBounds)
@@ -1124,7 +1125,8 @@ class Traverser(
 
                     if (upperBounds.any { it is GenericArrayType }) {
                         val errorTypes = upperBounds.filterIsInstance<GenericArrayType>()
-                        TODO("we do not support GenericArrayType yet, and $errorTypes found. SAT-1446")
+                        logger.warn { "we do not support GenericArrayType yet, and $errorTypes found. SAT-1446" }
+                        return
                     }
 
                     val upperBoundsTypes = typeResolver.intersectInheritors(upperBounds)

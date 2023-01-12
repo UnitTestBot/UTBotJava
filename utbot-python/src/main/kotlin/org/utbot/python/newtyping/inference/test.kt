88b20d1df188b4ffd68ee2e33e25401fe2d14cf8
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-01-12 12:28:05 +0300

    Added resolveTypeName function

--- utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/test.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/newtyping/inference/test.kt
@@ -5,10 +5,10 @@ import org.utbot.python.newtyping.pythonTypeRepresentation
 fun main() {
     TypeInferenceProcessor(
         "python3",
-        directoriesForSysPath = setOf("/home/tochilinak/Documents/projects/utbot/django-cms"),
-        "/home/tochilinak/Documents/projects/utbot/django-cms/cms/toolbar/items.py",
-        moduleOfSourceFile = "cms.toolbar.items",
-        "may_be_lazy"
+        directoriesForSysPath = setOf("/home/tochilinak/Documents/projects/utbot/UTBotJava/utbot-python/samples/easy_samples"),
+        "/home/tochilinak/Documents/projects/utbot/UTBotJava/utbot-python/samples/easy_samples/general.py",
+        moduleOfSourceFile = "general",
+        "fact"
     ).inferTypes(cancel = { false }).forEach {
         println(it.pythonTypeRepresentation())
     }
--- utbot-python/src/main/kotlin/org/utbot/python/newtyping/mypy/GlobalNamesStorage.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/newtyping/mypy/GlobalNamesStorage.kt
@@ -1,6 +1,33 @@
 package org.utbot.python.newtyping.mypy
 
-class GlobalNamesStorage {
+import org.utbot.python.newtyping.general.Type
+
+class GlobalNamesStorage(private val mypyStorage: MypyAnnotationStorage) {
+    fun resolveTypeName(module: String, name: String): Type? {
+        val splitName = name.split(".")
+        if (splitName.size == 1) {
+            val nameFromStorage = mypyStorage.names[module]?.find { it.name == name } ?: return null
+            return when (nameFromStorage) {
+                is LocalTypeName -> mypyStorage.definitions[module]!![name]!!.annotation.asUtBotType
+                is ImportedTypeName -> {
+                    val split = nameFromStorage.fullname.split(".")
+                    resolveTypeName(split.dropLast(1).joinToString("."), split.last())
+                }
+                else -> null
+            }
+        }
+        val withoutLast = splitName.dropLast(1)
+        withoutLast.foldRight(Pair(withoutLast.joinToString("."), splitName.last())) { nextPart, (left, right) ->
+            val next = Pair(left.dropLast(nextPart.length).removeSuffix("."), "$nextPart.$right")
+            val nameFromStorage = (mypyStorage.names[module]?.find { it.name == left } as? ModuleName)
+                ?: return@foldRight next
+            val attempt = resolveTypeName(nameFromStorage.fullname, right)
+            if (attempt != null)
+                return attempt
+            next
+        }
+        return null
+    }
 }
 
 sealed class Name(val name: String)

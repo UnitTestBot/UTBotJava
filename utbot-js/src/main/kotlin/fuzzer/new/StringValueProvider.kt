d021bbc780781a98c4cfecc103a35c8568efd702
Vladislav Kasimov <vladislavkasimov@gmail.com>
2023-01-23 14:38:55 +0300

    Implement `StringValueProvider`

--- utbot-js/src/main/kotlin/fuzzer/new/StringValueProvider.kt
+++ utbot-js/src/main/kotlin/fuzzer/new/StringValueProvider.kt
@@ -1,11 +1,14 @@
 package fuzzer.new
 
 import framework.api.js.JsClassId
+import framework.api.js.JsPrimitiveModel
 import framework.api.js.util.isJsBasic
 import framework.api.js.util.jsStringClassId
 import org.utbot.fuzzer.FuzzedValue
+import org.utbot.fuzzer.providers.PrimitivesModelProvider.fuzzed
 import org.utbot.fuzzing.Seed
 import org.utbot.fuzzing.ValueProvider
+import org.utbot.fuzzing.seeds.StringValue
 
 object StringValueProvider : ValueProvider<JsClassId, FuzzedValue, JsMethodDescription> {
 
@@ -16,7 +19,25 @@ object StringValueProvider : ValueProvider<JsClassId, FuzzedValue, JsMethodDescr
     override fun generate(
         description: JsMethodDescription,
         type: JsClassId
-    ): Sequence<Seed<JsClassId, FuzzedValue>> {
-        TODO("Not yet implemented")
+    ): Sequence<Seed<JsClassId, FuzzedValue>> = sequence {
+        val constants = description.concreteValues.asSequence()
+            .filter { it.type == jsStringClassId }
+        val values = constants
+            .mapNotNull { it.value as? String } +
+                sequenceOf("", "abc", "\n\t\r")
+        values.forEach { value ->
+            yield(Seed.Known(StringValue(value)) { known ->
+                JsPrimitiveModel(known.toString()).fuzzed {
+                    summary = "%var% = '${known.toString().substringToLength(10, "...")}'"
+                }
+            })
+        }
+    }
+
+    private fun String.substringToLength(size: Int, postfix: String): String {
+        return when {
+            length <= size -> this
+            else -> substring(0, size) + postfix
+        }
     }
 }

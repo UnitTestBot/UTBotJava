7d9a6f6b050ddf92f57af78034b8de15ff888b5b
Sergey Loktev <loktevsergay.2003@gmail.com>
2023-02-13 17:38:04 +0300

    Implemented JavaScript exports manager based on package.json

--- utbot-js/src/main/kotlin/utils/ExportsProvider.kt
+++ utbot-js/src/main/kotlin/utils/ExportsProvider.kt
@@ -6,7 +6,7 @@ object ExportsProvider {
 
     fun getExportsRegex(packageJson: PackageJson): Regex = with(packageJson) {
         when (isModule) {
-            true -> Regex("")
+            true -> Regex("(.*)")
             false -> Regex("exports[.](.*) =")
         }
     }
@@ -14,6 +14,27 @@ object ExportsProvider {
     fun getExportsDelimiter(packageJson: PackageJson): String = with(packageJson) {
         when(isModule) {
             true -> ","
+            false -> "\n"
+        }
+    }
+
+    fun getExportsFrame(exportString: String, packageJson: PackageJson): String = with(packageJson) {
+        when(isModule) {
+            true -> exportString
+            false -> "exports.$exportString = $exportString"
+        }
+    }
+
+    fun getExportsPrefix(packageJson: PackageJson): String = with(packageJson) {
+        when(isModule) {
+            true -> "\nexport {"
+            false -> "\n"
+        }
+    }
+
+    fun getExportsPostfix(packageJson: PackageJson): String = with(packageJson) {
+        when(isModule) {
+            true -> "}\n"
             false -> "\n"
         }
     }

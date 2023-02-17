846c80ecd2614757c156998e219e34e7fc71993e
Vladislav Kasimov <vladislavkasimov@gmail.com>
2023-02-17 12:18:21 +0300

    Fix JS compilation error

--- utbot-js/src/main/kotlin/framework/codegen/model/constructor/tree/JsCgVariableConstructor.kt
+++ utbot-js/src/main/kotlin/framework/codegen/model/constructor/tree/JsCgVariableConstructor.kt
@@ -1,10 +1,5 @@
 package framework.codegen.model.constructor.tree
 
-import org.utbot.framework.plugin.api.UtArrayModel
-import org.utbot.framework.plugin.api.UtAssembleModel
-import org.utbot.framework.plugin.api.UtCompositeModel
-import org.utbot.framework.plugin.api.UtModel
-import org.utbot.framework.plugin.api.UtReferenceModel
 import framework.api.js.JsPrimitiveModel
 import org.utbot.framework.codegen.domain.context.CgContext
 import org.utbot.framework.codegen.domain.models.CgLiteral
@@ -12,6 +7,8 @@ import org.utbot.framework.codegen.domain.models.CgValue
 import org.utbot.framework.codegen.tree.CgComponents
 import org.utbot.framework.codegen.tree.CgVariableConstructor
 import org.utbot.framework.codegen.util.nullLiteral
+import org.utbot.framework.plugin.api.UtModel
+import org.utbot.framework.plugin.api.UtReferenceModel
 
 class JsCgVariableConstructor(ctx: CgContext) : CgVariableConstructor(ctx) {
 
@@ -20,12 +17,8 @@ class JsCgVariableConstructor(ctx: CgContext) : CgVariableConstructor(ctx) {
     override fun getOrCreateVariable(model: UtModel, name: String?): CgValue {
         val baseName = name ?: nameGenerator.nameFrom(model.classId)
         return if (model is UtReferenceModel) valueByModelId.getOrPut(model.id) {
-            when (model) {
-                is UtCompositeModel -> TODO()
-                is UtAssembleModel -> constructAssemble(model, baseName)
-                is UtArrayModel -> TODO()
-                else -> TODO()
-            }
+            // TODO SEVERE: May lead to unexpected behavior in case of changes to the original method
+            super.getOrCreateVariable(model, name)
         } else valueByModel.getOrPut(model) {
             when (model) {
                 is JsPrimitiveModel -> CgLiteral(model.classId, model.value)

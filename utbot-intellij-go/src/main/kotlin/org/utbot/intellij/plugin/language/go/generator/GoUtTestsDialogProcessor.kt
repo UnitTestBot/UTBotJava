fadba72e457e65cd2c302e6989756d2314adc02d
egiptipavel <57724222+egiptipavel@users.noreply.github.com>
2023-02-27 23:12:40 +0300

    Go plugin fixes (#1851)
    * Delete project SDK field from UnitTestBot action dialog. Add auto detection of project SDK and error dialogs when SDK isn't setup or version less than 1.18
    
    * Run plugin action from context menu

--- utbot-intellij-go/src/main/kotlin/org/utbot/intellij/plugin/language/go/generator/GoUtTestsDialogProcessor.kt
+++ utbot-intellij-go/src/main/kotlin/org/utbot/intellij/plugin/language/go/generator/GoUtTestsDialogProcessor.kt
@@ -1,36 +1,74 @@
 package org.utbot.intellij.plugin.language.go.generator
 
 import com.goide.psi.GoFunctionOrMethodDeclaration
+import com.goide.sdk.GoSdk
+import com.goide.sdk.GoSdkService
+import com.goide.sdk.GoSdkVersion
 import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
 import org.utbot.go.logic.GoUtTestsGenerationConfig
 import org.utbot.intellij.plugin.language.go.models.GenerateGoTestsModel
 import org.utbot.intellij.plugin.language.go.ui.GenerateGoTestsDialogWindow
+import org.utbot.intellij.plugin.language.go.ui.utils.resolveGoExecutablePath
 
 object GoUtTestsDialogProcessor {
 
     fun createDialogAndGenerateTests(
         project: Project,
+        module: Module,
         targetFunctions: Set<GoFunctionOrMethodDeclaration>,
         focusedTargetFunctions: Set<GoFunctionOrMethodDeclaration>,
     ) {
-        val dialogProcessor = createDialog(project, targetFunctions, focusedTargetFunctions)
-        if (!dialogProcessor.showAndGet()) return
-
-        createTests(dialogProcessor.model)
+        createDialog(project, module, targetFunctions, focusedTargetFunctions)?.let {
+            if (it.showAndGet()) createTests(it.model)
+        }
     }
 
     private fun createDialog(
         project: Project,
+        module: Module,
         targetFunctions: Set<GoFunctionOrMethodDeclaration>,
         focusedTargetFunctions: Set<GoFunctionOrMethodDeclaration>,
-    ): GenerateGoTestsDialogWindow {
+    ): GenerateGoTestsDialogWindow? {
+        val goSdk = GoSdkService.getInstance(project).getSdk(module)
+        if (goSdk == GoSdk.NULL) {
+            val result = Messages.showOkCancelDialog(
+                project,
+                "GOROOT is not defined. Select it?",
+                "Unsupported Go SDK",
+                "Select",
+                "Cancel",
+                Messages.getErrorIcon()
+            )
+            if (result == Messages.OK) {
+                ShowSettingsUtil.getInstance().showSettingsDialog(project, "GOROOT")
+            }
+            return null
+        } else if (!goSdk.isValid || GoSdkVersion.fromText(goSdk.version).isLessThan(GoSdkVersion.GO_1_18)) {
+            val result = Messages.showOkCancelDialog(
+                project,
+                "Go SDK isn't valid or version less than 1.18. Select another SDK?",
+                "Unsupported Go SDK",
+                "Select",
+                "Cancel",
+                Messages.getErrorIcon()
+            )
+            if (result == Messages.OK) {
+                ShowSettingsUtil.getInstance().showSettingsDialog(project, "GOROOT")
+            }
+            return null
+        }
+
         return GenerateGoTestsDialogWindow(
             GenerateGoTestsModel(
                 project,
+                goSdk.resolveGoExecutablePath()!!,
                 targetFunctions,
                 focusedTargetFunctions,
             )
@@ -51,8 +89,7 @@ object GoUtTestsDialogProcessor {
                 )
 
                 IntellijGoUtTestsGenerationController(model, indicator).generateTests(
-                    selectedFunctionsNamesBySourceFiles,
-                    testsGenerationConfig
+                    selectedFunctionsNamesBySourceFiles, testsGenerationConfig
                 ) { indicator.isCanceled }
             }
         })

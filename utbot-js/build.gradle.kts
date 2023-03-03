4fb92f2d861d89cd12f5073c9c664ccc1a3bdebb
Sergey Loktev <loktevsergay.2003@gmail.com>
2023-03-03 18:08:10 +0300

    Removed extra dep

--- utbot-js/build.gradle.kts
+++ utbot-js/build.gradle.kts
@@ -29,7 +29,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     api(project(":utbot-framework"))
-    api(project(":utbot-fuzzers"))
     // https://mvnrepository.com/artifact/com.google.javascript/closure-compiler
     implementation("com.google.javascript:closure-compiler:v20221102")
 

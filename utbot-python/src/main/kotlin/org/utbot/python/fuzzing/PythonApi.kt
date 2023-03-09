1b7affe97e2d74cf4ae0f91bac9cbdf77fd9fb43
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2023-03-09 15:12:12 +0300

    Fix logging in python fuzzing and remove unused code

--- utbot-python/src/main/kotlin/org/utbot/python/fuzzing/PythonApi.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/fuzzing/PythonApi.kt
@@ -4,8 +4,6 @@ import mu.KotlinLogging
 import org.utbot.framework.plugin.api.Instruction
 import org.utbot.framework.plugin.api.UtError
 import org.utbot.fuzzer.FuzzedContext
-import org.utbot.fuzzer.IdGenerator
-import org.utbot.fuzzer.IdentityPreservingIdGenerator
 import org.utbot.fuzzer.UtFuzzedExecution
 import org.utbot.fuzzing.Configuration
 import org.utbot.fuzzing.Control
@@ -23,8 +21,6 @@ import org.utbot.python.newtyping.PythonSubtypeChecker
 import org.utbot.python.newtyping.PythonTypeStorage
 import org.utbot.python.newtyping.general.Type
 import org.utbot.python.newtyping.pythonTypeRepresentation
-import java.util.*
-import java.util.concurrent.atomic.AtomicLong
 
 private val logger = KotlinLogging.logger {}
 
@@ -63,7 +59,7 @@ class PythonFuzzedValue(
     val summary: String? = null,
 )
 
-fun pythonDefaultValueProviders(idGenerator: IdGenerator<Long>) = listOf(
+fun pythonDefaultValueProviders() = listOf(
     NoneValueProvider,
     BoolValueProvider,
     IntValueProvider,
@@ -88,39 +84,38 @@ class PythonFuzzing(
     val execute: suspend (description: PythonMethodDescription, values: List<PythonFuzzedValue>) -> PythonFeedback,
 ) : Fuzzing<Type, PythonFuzzedValue, PythonMethodDescription, PythonFeedback> {
 
-    private fun generateDefault(description: PythonMethodDescription, type: Type, idGenerator: IdGenerator<Long>): Sequence<Seed<Type, PythonFuzzedValue>> {
+    private fun generateDefault(description: PythonMethodDescription, type: Type): Sequence<Seed<Type, PythonFuzzedValue>> {
         var providers = emptyList<Seed<Type, PythonFuzzedValue>>().asSequence()
-        pythonDefaultValueProviders(idGenerator).asSequence().forEach { provider ->
+        pythonDefaultValueProviders().asSequence().forEach { provider ->
             if (provider.accept(type)) {
-                logger.info { "Provider ${provider.javaClass.simpleName} accepts type ${type.pythonTypeRepresentation()}" }
+                logger.debug { "Provider ${provider.javaClass.simpleName} accepts type ${type.pythonTypeRepresentation()}" }
                 providers += provider.generate(description, type)
             }
         }
         return providers
     }
 
-    private fun generateSubtype(description: PythonMethodDescription, type: Type, idGenerator: IdGenerator<Long>): Sequence<Seed<Type, PythonFuzzedValue>> {
+    private fun generateSubtype(description: PythonMethodDescription, type: Type): Sequence<Seed<Type, PythonFuzzedValue>> {
         var providers = emptyList<Seed<Type, PythonFuzzedValue>>().asSequence()
         if (type.meta is PythonProtocolDescription) {
             val subtypes = pythonTypeStorage.allTypes.filter {
                 PythonSubtypeChecker.checkIfRightIsSubtypeOfLeft(type, it, pythonTypeStorage)
             }
             subtypes.forEach {
-                providers += generateDefault(description, it, idGenerator)
+                providers += generateDefault(description, it)
             }
         }
         return providers
     }
 
     override fun generate(description: PythonMethodDescription, type: Type): Sequence<Seed<Type, PythonFuzzedValue>> {
-        val idGenerator = PythonIdGenerator()
         var providers = emptyList<Seed<Type, PythonFuzzedValue>>().asSequence()
 
         if (type.isAny()) {
-            logger.info("Any does not have provider")
+            logger.debug("Any does not have provider")
         } else {
-            providers += generateDefault(description, type, idGenerator)
-            providers += generateSubtype(description, type, idGenerator)
+            providers += generateDefault(description, type)
+            providers += generateSubtype(description, type)
         }
 
         return providers
@@ -136,22 +131,5 @@ class PythonFuzzing(
         configuration: Configuration
     ) {
         super.update(description, statistic, configuration)
-    }
-}
-
-class PythonIdGenerator(lowerBound: Long = DEFAULT_LOWER_BOUND) : IdentityPreservingIdGenerator<Long> {
-    private val lastId: AtomicLong = AtomicLong(lowerBound)
-    private val cache: IdentityHashMap<Any?, Long> = IdentityHashMap()
-
-    override fun getOrCreateIdForValue(value: Any): Long {
-        return cache.getOrPut(value) { createId() }
-    }
-
-    override fun createId(): Long {
-        return lastId.incrementAndGet()
-    }
-
-    companion object {
-        const val DEFAULT_LOWER_BOUND: Long = 1500_000_000
     }
 }

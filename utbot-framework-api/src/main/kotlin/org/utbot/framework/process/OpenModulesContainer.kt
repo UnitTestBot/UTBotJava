fa787ae7b80a2f2fd1747095d820a347fdcf123c
Artemii.Kononov <artemkononov2@gmail.com>
2023-03-10 21:18:39 +0300

    add more open packages

--- utbot-framework-api/src/main/kotlin/org/utbot/framework/process/OpenModulesContainer.kt
+++ utbot-framework-api/src/main/kotlin/org/utbot/framework/process/OpenModulesContainer.kt
@@ -11,6 +11,7 @@ object OpenModulesContainer {
     init {
         modulesContainer = buildList {
             openPackage("java.base", "sun.security.util")
+            openPackage("java.base", "java.text")
             openPackage("java.base", "jdk.internal.misc")
             openPackage("java.base", "sun.reflect.generics.repository")
             openPackage("java.base", "java.lang")

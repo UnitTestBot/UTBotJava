9defc401fbf1849a173f72118cb86ea6445e3bb8
IlyaMuravjov <muravjovilya@gmail.com>
2023-03-13 15:47:27 +0300

    Implement BaseTestsModel.getSortedSpringConfigurationClasses()

--- utbot-ui-commons/src/main/kotlin/org/utbot/intellij/plugin/models/BaseTestModel.kt
+++ utbot-ui-commons/src/main/kotlin/org/utbot/intellij/plugin/models/BaseTestModel.kt
@@ -1,15 +1,24 @@
 package org.utbot.intellij.plugin.models
 
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.TestModuleProperties
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.newvfs.impl.FakeVirtualFile
+import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiClass
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.ProjectScope
+import com.intellij.psi.search.searches.AnnotatedElementsSearch
 import org.jetbrains.kotlin.idea.core.getPackage
 import org.jetbrains.kotlin.idea.util.projectStructure.allModules
+import org.jetbrains.kotlin.idea.util.rootManager
+import org.jetbrains.kotlin.idea.util.sourceRoot
 import org.utbot.framework.plugin.api.CodegenLanguage
 import org.utbot.intellij.plugin.ui.utils.ITestSourceRoot
+import org.utbot.intellij.plugin.ui.utils.getSortedTestRoots
 import org.utbot.intellij.plugin.ui.utils.isBuildWithGradle
 import org.utbot.intellij.plugin.ui.utils.suitableTestSourceRoots
 
@@ -53,6 +62,42 @@ open class BaseTestsModel(
     fun getAllTestSourceRoots() : MutableList<out ITestSourceRoot> {
         with(if (project.isBuildWithGradle) project.allModules() else potentialTestModules) {
             return this.flatMap { it.suitableTestSourceRoots().toList() }.toMutableList().distinct().toMutableList()
+        }
+    }
+
+    fun getSortedTestRoots(): MutableList<ITestSourceRoot> = getSortedTestRoots(
+        getAllTestSourceRoots(),
+        sourceRootHistory,
+        srcModule.rootManager.sourceRoots.map { file: VirtualFile -> file.toNioPath().toString() },
+        codegenLanguage
+    )
+
+    fun getSortedSpringConfigurationClasses(): List<PsiClass> {
+        val psiFacade = JavaPsiFacade.getInstance(project)
+        val testRootToIndex = getSortedTestRoots().withIndex().associate { (i, root) -> root.dir to i }
+
+        // not using `srcModule.testModules(project)` here because it returns
+        // test modules for dependencies of source module if no test roots are found
+        // for the source module itself, however we don't want to search for
+        // configurations there because they are unlikely to be of any use
+        val testModules = ModuleManager.getInstance(project).modules.filter { module ->
+            srcModule == TestModuleProperties.getInstance(module).productionModule
+        }
+
+        val searchScope = testModules.fold(GlobalSearchScope.moduleScope(srcModule)) { accScope, module ->
+            accScope.union(GlobalSearchScope.moduleScope(module))
+        }
+
+        return listOf(
+            "org.springframework.boot.test.context.TestConfiguration",
+            "org.springframework.context.annotation.Configuration"
+        ).mapNotNull {
+            psiFacade.findClass(it, ProjectScope.getLibrariesScope(project))
+        }.flatMap { annotation ->
+            AnnotatedElementsSearch
+                .searchPsiClasses(annotation, searchScope)
+                .findAll()
+                .sortedBy { testRootToIndex[it.containingFile.sourceRoot] ?: Int.MAX_VALUE }
         }
     }
 

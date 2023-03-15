d661c75075ba7cf3726464bc5b7a1459e1a7946c
Egor Kulikov <egor.k.kulikov@gmail.com>
2023-03-15 15:55:46 +0300

    Update target compatibility

--- utbot-spring-analyzer/build.gradle.kts
+++ utbot-spring-analyzer/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {

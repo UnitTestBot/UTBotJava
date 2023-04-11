a6b5ff72ec88ef2126c0449e0133dee28765b8c0
Andrey Tarbeev <54685068+sofurihafe@users.noreply.github.com>
2023-04-11 11:24:19 +0300

    Fix not defined variables in test method in Spring project #2107 (#2124)

--- utbot-framework/src/main/kotlin/org/utbot/framework/codegen/tree/CgSpringTestClassConstructor.kt
+++ utbot-framework/src/main/kotlin/org/utbot/framework/codegen/tree/CgSpringTestClassConstructor.kt
@@ -24,7 +24,6 @@ import org.utbot.framework.codegen.domain.models.ClassModels
 import org.utbot.framework.codegen.domain.models.SpringTestClassModel
 import org.utbot.framework.plugin.api.ClassId
 import org.utbot.framework.plugin.api.UtCompositeModel
-import org.utbot.framework.plugin.api.UtModel
 import org.utbot.framework.plugin.api.util.id
 import org.utbot.framework.plugin.api.util.objectClassId
 
@@ -44,6 +43,8 @@ class CgSpringTestClassConstructor(context: CgContext): CgAbstractTestClassConst
             if (mockedFields.isNotEmpty()) {
                 fields += constructClassFields(testClassModel.injectedMockModels, injectMocksClassId)
                 fields += mockedFields
+
+                clearUnwantedVariableModels()
 
                 val (closeableField, closeableMethods) = constructMockitoCloseables()
                 fields += closeableField
@@ -115,6 +116,28 @@ class CgSpringTestClassConstructor(context: CgContext): CgAbstractTestClassConst
         }
 
         return constructedDeclarations
+    }
+
+    /**
+     * Clears the results of variable instantiations that occured
+     * when we create class variables with specific annotations.
+     * Actually, only mentioned variables should be stored in `valueByModelId`.
+     *
+     * This is a kind of HACK.
+     * It is better to distinguish creating variable by model with all
+     * related side effects and just creating a variable definition,
+     * but it will take very long time to do it now.
+     */
+    private fun clearUnwantedVariableModels() {
+        val whiteListOfModels =
+            listOf(
+                variableConstructor.mockedModelsVariables,
+                variableConstructor.injectedMocksModelsVariables
+            ).flatMap { modelSet -> modelSet.keys.map { models -> context.getIdByModel(models.first()) } }
+
+        valueByModelId
+            .filter { it.key !in whiteListOfModels }
+            .forEach { valueByModelId.remove(it.key) }
     }
 
     private fun constructMockitoCloseables(): Pair<CgFieldDeclaration, CgMethodsCluster> {

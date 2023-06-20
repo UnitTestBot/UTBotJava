4bbaad2306e9fe214f67b9c9045d1914c09bc5fb
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2023-06-20 18:01:01 +0300

    Support fuzzing for regular expressions

--- utbot-python/src/main/kotlin/org/utbot/python/fuzzing/provider/StrValueProvider.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/fuzzing/provider/StrValueProvider.kt
@@ -3,6 +3,7 @@ package org.utbot.python.fuzzing.provider
 import org.utbot.fuzzing.Seed
 import org.utbot.fuzzing.ValueProvider
 import org.utbot.fuzzing.seeds.KnownValue
+import org.utbot.fuzzing.seeds.RegexValue
 import org.utbot.fuzzing.seeds.StringValue
 import org.utbot.python.framework.api.python.PythonTree
 import org.utbot.python.framework.api.python.util.pythonStrClassId
@@ -10,31 +11,45 @@ import org.utbot.python.fuzzing.PythonFuzzedConcreteValue
 import org.utbot.python.fuzzing.PythonFuzzedValue
 import org.utbot.python.fuzzing.PythonMethodDescription
 import org.utbot.python.fuzzing.provider.utils.generateSummary
+import org.utbot.python.fuzzing.provider.utils.isPattern
 import org.utbot.python.fuzzing.provider.utils.transformQuotationMarks
+import org.utbot.python.fuzzing.provider.utils.transformRawString
 import org.utbot.python.newtyping.general.Type
 import org.utbot.python.newtyping.pythonTypeName
+import kotlin.random.Random
 
 object StrValueProvider : ValueProvider<Type, PythonFuzzedValue, PythonMethodDescription> {
     override fun accept(type: Type): Boolean {
         return type.pythonTypeName() == pythonStrClassId.canonicalName
     }
 
-    private fun getStrConstants(concreteValues: Collection<PythonFuzzedConcreteValue>): List<StringValue> {
+    private fun getStrConstants(concreteValues: Collection<PythonFuzzedConcreteValue>): List<String> {
         return concreteValues
             .filter { accept(it.type) }
             .map {
-                StringValue((it.value as String).transformQuotationMarks())
+                val value = it.value as String
+                value.transformRawString()
+            }
+            .map {
+                it.transformQuotationMarks()
             }
     }
 
     override fun generate(description: PythonMethodDescription, type: Type) = sequence {
         val strConstants = getStrConstants(description.concreteValues) + listOf(
-            StringValue("pythön"),
-            StringValue("abcdefghijklmnopqrst"),
-            StringValue("foo"),
-            StringValue("€"),
+            "pythön",
+            "foo",
+            "\t\n\r",
         )
-        strConstants.forEach { yieldStrings(it) { value } }
+        strConstants.forEach { yieldStrings(StringValue(it)) { value } }
+
+        strConstants
+            .filter {
+                it.isPattern()
+            }
+            .forEach {
+                yieldStrings(RegexValue(it, Random(0)), StringValue::value)
+            }
     }
 
     private suspend fun <T : KnownValue<T>> SequenceScope<Seed<Type, PythonFuzzedValue>>.yieldStrings(value: T, block: T.() -> Any) {
--- utbot-python/src/main/kotlin/org/utbot/python/fuzzing/provider/utils/StringUtils.kt
+++ utbot-python/src/main/kotlin/org/utbot/python/fuzzing/provider/utils/StringUtils.kt
@@ -1,9 +1,12 @@
 package org.utbot.python.fuzzing.provider.utils
 
+import java.util.regex.Pattern
+import java.util.regex.PatternSyntaxException
+
 fun String.transformQuotationMarks(): String {
 
     val doubleQuotationMarks = this.startsWith("\"") && this.endsWith("\"")
-    val oneQuotationMarks = this.startsWith("'") && this.endsWith("'")
+//    val oneQuotationMarks = this.startsWith("'") && this.endsWith("'")
 
     val tripleDoubleQuotationMarks = this.startsWith("\"\"\"") && this.endsWith("\"\"\"")
     val tripleOneQuotationMarks = this.startsWith("'''") && this.endsWith("'''")
@@ -12,9 +15,25 @@ fun String.transformQuotationMarks(): String {
         return this.drop(3).dropLast(3)
     }
 
-    if (oneQuotationMarks || doubleQuotationMarks) {
+    if (doubleQuotationMarks) {
         return this.drop(1).dropLast(1)
     }
 
     return this
+}
+
+fun String.transformRawString(): String {
+    return if (Pattern.matches("r\".*\"", this) || Pattern.matches("r'.*'", this)) {
+        this.drop(2).dropLast(1)
+    } else {
+        this
+    }
+}
+
+fun String.isPattern(): Boolean {
+    return try {
+        Pattern.compile(this); true
+    } catch (_: PatternSyntaxException) {
+        false
+    }
 }

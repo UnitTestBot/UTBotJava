d80b4e1bb5cbdb5b22742e224cc2f0519e903985
Egor Kulikov <egor.k.kulikov@gmail.com>
2023-06-23 03:25:36 +0300

    Do not lie that you need this variable

--- utbot-framework/src/main/kotlin/org/utbot/framework/codegen/tree/CgSpringIntegrationTestClassConstructor.kt
+++ utbot-framework/src/main/kotlin/org/utbot/framework/codegen/tree/CgSpringIntegrationTestClassConstructor.kt
@@ -10,11 +10,6 @@ import org.utbot.framework.plugin.api.UtSpringContextModel
 class CgSpringIntegrationTestClassConstructor(context: CgContext) : CgAbstractSpringTestClassConstructor(context) {
 
     override fun constructClassFields(testClassModel: SpringTestClassModel): List<CgFieldDeclaration> {
-        // NOTE that it is important to construct applicationContext variable
-        // even though it is not used in generated code
-        // because it is used in some assemble models as an instance of instantiation call.
-        variableConstructor.getOrCreateVariable(UtSpringContextModel)
-
         val autowiredFromContextModels = testClassModel.springSpecificInformation.autowiredFromContextModels
         return constructFieldsWithAnnotation(autowiredClassId, autowiredFromContextModels)
     }

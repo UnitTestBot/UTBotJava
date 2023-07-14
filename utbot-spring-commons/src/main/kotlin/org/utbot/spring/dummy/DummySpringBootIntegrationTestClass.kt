ab5897e5f2aa121a3d69d5832cb01598b833fa06
IlyaMuravjov <muravjovilya@gmail.com>
2023-07-14 21:36:54 +0300

    Fix `DummySpringBootIntegrationTestClass` annotations

--- utbot-spring-commons/src/main/kotlin/org/utbot/spring/dummy/DummySpringBootIntegrationTestClass.kt
+++ utbot-spring-commons/src/main/kotlin/org/utbot/spring/dummy/DummySpringBootIntegrationTestClass.kt
@@ -1,11 +1,13 @@
 package org.utbot.spring.dummy
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTestContextBootstrapper
 import org.springframework.test.context.BootstrapWith
 
+@SpringBootTest
 @BootstrapWith(SpringBootTestContextBootstrapper::class)
-class DummySpringBootIntegrationTestClass : DummySpringIntegrationTestClass()
+open class DummySpringBootIntegrationTestClass : DummySpringIntegrationTestClass()
 
 @AutoConfigureTestDatabase
-class DummySpringBootIntegrationTestClassAutoconfigTestDB : DummySpringIntegrationTestClass()
+class DummySpringBootIntegrationTestClassAutoconfigTestDB : DummySpringBootIntegrationTestClass()

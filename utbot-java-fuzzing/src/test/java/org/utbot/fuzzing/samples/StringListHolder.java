5b4d643b6d50415ff569301dd46ff1336be97888
IlyaMuravjov <71839386+IlyaMuravjov@users.noreply.github.com>
2023-07-21 15:28:46 +0300

    Make fuzzer use generic field types #2430 (#2431)

---
+++ utbot-java-fuzzing/src/test/java/org/utbot/fuzzing/samples/StringListHolder.java
@@ -0,0 +1,20 @@
+package org.utbot.fuzzing.samples;
+
+import java.util.List;
+
+public class StringListHolder {
+    private List<String> strings;
+
+
+    public List<String> getStrings() {
+        return strings;
+    }
+
+
+    public void setStrings(List<String> strings) {
+        this.strings = strings;
+    }
+
+
+    public void methodUnderTest() {}
+}

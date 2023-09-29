825b7867e9c791eadc3ce927214afa563d2cc4b9
IlyaMuravjov <muravjovilya@gmail.com>
2023-09-29 13:13:45 +0300

    Automatically install `spring-security-test` when `spring-security-core` is present

--- utbot-spring-framework/src/main/kotlin/org/utbot/framework/plugin/api/utils/SpringDependencyPatterns.kt
+++ utbot-spring-framework/src/main/kotlin/org/utbot/framework/plugin/api/utils/SpringDependencyPatterns.kt
@@ -1,17 +1,18 @@
 package org.utbot.framework.plugin.api.utils
 
-import org.utbot.framework.codegen.domain.SpringModule.SPRING_BEANS
-import org.utbot.framework.codegen.domain.SpringModule.SPRING_BOOT
 import org.utbot.framework.codegen.domain.SpringModule
+import org.utbot.framework.codegen.domain.SpringModule.*
 
 fun SpringModule.patterns(): Patterns {
     val moduleLibraryPatterns = when (this) {
         SPRING_BOOT -> springBootModulePatterns
         SPRING_BEANS -> springBeansModulePatterns
+        SPRING_SECURITY -> springSecurityModulePatterns
     }
     val libraryPatterns = when (this) {
         SPRING_BOOT -> springBootPatterns
         SPRING_BEANS -> springBeansPatterns
+        SPRING_SECURITY -> springSecurityPatterns
     }
 
     return Patterns(moduleLibraryPatterns, libraryPatterns)
@@ -21,10 +22,12 @@ fun SpringModule.testPatterns(): Patterns {
     val moduleLibraryPatterns = when (this) {
         SPRING_BOOT -> springBootTestModulePatterns
         SPRING_BEANS -> springBeansTestModulePatterns
+        SPRING_SECURITY -> springSecurityTestModulePatterns
     }
     val libraryPatterns = when (this) {
         SPRING_BOOT -> springBootTestPatterns
         SPRING_BEANS -> springBeansTestPatterns
+        SPRING_SECURITY -> springSecurityTestPatterns
     }
 
     return Patterns(moduleLibraryPatterns, libraryPatterns)
@@ -58,3 +61,17 @@ val springBootTestPatterns = listOf(SPRING_BOOT_TEST_JAR_PATTERN, SPRING_BOOT_TE
 
 val SPRING_BOOT_TEST_BASIC_MODULE_PATTERN = Regex("spring-boot-test")
 val springBootTestModulePatterns = listOf(SPRING_BOOT_TEST_BASIC_MODULE_PATTERN)
+
+val SPRING_SECURITY_JAR_PATTERN = Regex("spring-security-core-([0-9]+)(\\.[0-9]+){1,2}")
+val SPRING_SECURITY_MVN_PATTERN = Regex("org\\.springframework\\.security:spring-security-core:([0-9]+)(\\.[0-9]+){1,2}")
+val springSecurityPatterns = listOf(SPRING_SECURITY_JAR_PATTERN, SPRING_SECURITY_MVN_PATTERN)
+
+val SPRING_SECURITY_BASIC_MODULE_PATTERN = Regex("spring-security-core")
+val springSecurityModulePatterns = listOf(SPRING_SECURITY_BASIC_MODULE_PATTERN)
+
+val SPRING_SECURITY_TEST_JAR_PATTERN = Regex("spring-security-test-([0-9]+)(\\.[0-9]+){1,2}")
+val SPRING_SECURITY_TEST_MVN_PATTERN = Regex("org\\.springframework\\.security:spring-security-test:([0-9]+)(\\.[0-9]+){1,2}")
+val springSecurityTestPatterns = listOf(SPRING_SECURITY_TEST_JAR_PATTERN, SPRING_SECURITY_TEST_MVN_PATTERN)
+
+val SPRING_SECURITY_TEST_BASIC_MODULE_PATTERN = Regex("spring-security-test")
+val springSecurityTestModulePatterns = listOf(SPRING_SECURITY_TEST_BASIC_MODULE_PATTERN)

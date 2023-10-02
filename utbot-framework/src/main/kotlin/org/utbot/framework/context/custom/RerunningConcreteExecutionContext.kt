b42b698a074afbd6ecc2bb7968f549e078bc3f85
IlyaMuravjov <71839386+IlyaMuravjov@users.noreply.github.com>
2023-10-02 12:52:51 +0300

    Limit number of Spring context resets during reruns (#2626)

--- utbot-framework/src/main/kotlin/org/utbot/framework/context/custom/RerunningConcreteExecutionContext.kt
+++ utbot-framework/src/main/kotlin/org/utbot/framework/context/custom/RerunningConcreteExecutionContext.kt
@@ -13,6 +13,7 @@ import org.utbot.instrumentation.instrumentation.execution.UtExecutionInstrument
 
 class RerunningConcreteExecutionContext(
     private val delegateContext: ConcreteExecutionContext,
+    private val maxRerunsPerMethod: Int,
     private val rerunTimeoutInMillis: Long = 10L * UtSettings.concreteExecutionDefaultTimeoutInInstrumentedProcessMillis,
 ) : ConcreteExecutionContext by delegateContext {
     companion object {
@@ -23,31 +24,40 @@ class RerunningConcreteExecutionContext(
         executions: List<UtExecution>,
         methodUnderTest: ExecutableId,
         rerunExecutor: ConcreteExecutor<UtConcreteExecutionResult, UtExecutionInstrumentation>,
-    ): List<UtExecution> = delegateContext.transformExecutionsAfterMinimization(
-        executions,
-        methodUnderTest,
-        rerunExecutor
-    ).map { execution ->
-        runBlocking {
-            val result = try {
-                rerunExecutor.executeConcretely(
-                    methodUnderTest = methodUnderTest,
-                    stateBefore = execution.stateBefore,
-                    instrumentation = emptyList(),
-                    timeoutInMillis = rerunTimeoutInMillis,
-                    isRerun = true,
-                )
-            } catch (e: Throwable) {
-                // we can't update execution result if we don't have a result
-                logger.warn(e) { "Rerun failed, keeping original result for execution [$execution]" }
-                return@runBlocking execution
-            }
-            execution.copy(
-                stateBefore = result.stateBefore,
-                stateAfter = result.stateAfter,
-                result = result.result,
-                coverage = result.coverage,
-            )
-        }
+    ): List<UtExecution> {
+        @Suppress("NAME_SHADOWING")
+        val executions = delegateContext.transformExecutionsAfterMinimization(
+            executions,
+            methodUnderTest,
+            rerunExecutor
+        )
+            // it's better to rerun executions with non-empty coverage,
+            // because executions with empty coverage are often duplicated
+            .sortedBy { it.coverage?.coveredInstructions.isNullOrEmpty() }
+        return executions
+            .take(maxRerunsPerMethod)
+            .map { execution ->
+                runBlocking {
+                    val result = try {
+                        rerunExecutor.executeConcretely(
+                            methodUnderTest = methodUnderTest,
+                            stateBefore = execution.stateBefore,
+                            instrumentation = emptyList(),
+                            timeoutInMillis = rerunTimeoutInMillis,
+                            isRerun = true,
+                        )
+                    } catch (e: Throwable) {
+                        // we can't update execution result if we don't have a result
+                        logger.warn(e) { "Rerun failed, keeping original result for execution [$execution]" }
+                        return@runBlocking execution
+                    }
+                    execution.copy(
+                        stateBefore = result.stateBefore,
+                        stateAfter = result.stateAfter,
+                        result = result.result,
+                        coverage = result.coverage,
+                    )
+                }
+            } + executions.drop(maxRerunsPerMethod)
     }
 }

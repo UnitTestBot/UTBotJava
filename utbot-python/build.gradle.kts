dfc9ce856635747748c66beaab8217b60aa683e9
Ekaterina Tochilina <tochilina.2002@yandex.ru>
2023-11-09 17:24:19 +0300

    updated usvm-python-runner

--- utbot-python/build.gradle.kts
+++ utbot-python/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     api(project(":utbot-python-parser"))
     api(project(":utbot-python-types"))
     api(project(":utbot-python-executor"))
-    api("org.usvm:usvm-python-runner:b7c5189")
+    api("org.usvm:usvm-python-runner:1e2c90f")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
